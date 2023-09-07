3c26b6f8003cf2b5de4565e595e1a7f05ce7c142
Ramon Recuero <rrmoreno1987@gmail.com>
2023-09-07 15:06:17 +0900

    Kinto wallet test with userop helpers

--- test/KintoWallet.t.sol
+++ test/KintoWallet.t.sol
@@ -5,6 +5,7 @@ import "../src/wallet/KintoWallet.sol";
 import "../src/wallet/KintoWalletFactory.sol";
 
 import "@aa/interfaces/IAccount.sol";
+import "@aa/interfaces/INonceManager.sol";
 import "@aa/interfaces/IEntryPoint.sol";
 import "@aa/core/EntryPoint.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
@@ -15,6 +16,87 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
+abstract contract UserOpTest is Test {
+    using ECDSAUpgradeable for bytes32;
+
+    function packUserOp (UserOperation memory op, bool forSig) internal pure returns (bytes memory) {
+      if (forSig) {
+        return abi.encode(
+          op.sender,
+          op.nonce,
+          keccak256(op.initCode),
+          keccak256(op.callData),
+          op.callGasLimit,
+          op.verificationGasLimit,
+          op.preVerificationGas,
+          op.maxFeePerGas,
+          op.maxPriorityFeePerGas,
+          keccak256(op.paymasterAndData)
+        );
+      }
+      return abi.encode(
+        op.sender,
+        op.nonce,
+        op.initCode,
+        op.callData,
+        op.callGasLimit,
+        op.verificationGasLimit,
+        op.preVerificationGas,
+        op.maxFeePerGas,
+        op.maxPriorityFeePerGas,
+        op.paymasterAndData,
+        op.signature
+      );
+    }
+
+    function getUserOpHash (UserOperation memory op, IEntryPoint _entryPoint, uint256 chainID) internal pure returns (bytes32) {
+      bytes32 opHash = keccak256(packUserOp(op, true));
+      return keccak256(
+        abi.encode(
+          opHash,
+          address(_entryPoint),
+          chainID
+        )
+      );
+    }
+
+    function signUserOp (UserOperation memory op, IEntryPoint _entryPoint, uint256 chainID, uint256 privateKey) internal pure returns (bytes memory) {
+      bytes32 hash = getUserOpHash(op, _entryPoint, chainID);
+      hash = hash.toEthSignedMessageHash();
+      (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, hash);
+      bytes memory signature = abi.encodePacked(r, s, v);
+      return signature;
+    }
+    
+    // 'address', // sender
+    // 'uint256', // nonce
+    // 'bytes32', // initCode
+    // 'bytes32', // callData
+    // 'uint256', // callGasLimit
+    // 'uint256', // verificationGasLimit
+    // 'uint256', // preVerificationGas
+    // 'uint256', // maxFeePerGas
+    // 'uint256', // maxPriorityFeePerGas
+    // 'bytes32' // paymasterAndData
+    function createUserOperation(address _account, uint256 _privateKeyOwner) public view returns (UserOperation memory op) {
+      op = UserOperation({
+        sender: _account,
+        nonce: KintoWallet(payable(_account)).getNonce(),
+        initCode: bytes(""),
+        callData: bytes(""),
+        callGasLimit: 0,
+        verificationGasLimit: 150000, // default verification gas. will add create2 cost (3200+200*length) if initCode exists
+        preVerificationGas: 21000, // should also cover calldata cost.
+        maxFeePerGas: 0,
+        maxPriorityFeePerGas: 1e9,
+        paymasterAndData: bytes(""),
+        signature: bytes("")
+      });
+      op.signature = signUserOp(op, KintoWallet(payable(_account)).entryPoint(), 1, _privateKeyOwner);
+      return op;
+    }
+}
+
 contract KintoWalletv2 is KintoWallet {
   constructor(IEntryPoint _entryPoint) KintoWallet(_entryPoint) {}
 
@@ -24,7 +106,7 @@ contract KintoWalletv2 is KintoWallet {
   }
 }
 
-contract KintoIDTest is Test {
+contract KintoIDTest is UserOpTest {
     using ECDSAUpgradeable for bytes32;
     using SignatureChecker for address;
 
@@ -34,6 +116,8 @@ contract KintoIDTest is Test {
     KintoWallet _kintoWalletv1;
     KintoWalletv2 _kintoWalletv2;
 
+    uint256 chainID = 1;
+
     address _owner = address(1);
     address _secondowner = address(2);
     address _user = vm.addr(3);
@@ -41,7 +125,7 @@ contract KintoIDTest is Test {
     address _upgrader = address(5);
 
     function setUp() public {
-        vm.chainId(1);
+        vm.chainId(chainID);
         vm.startPrank(_owner);
         _entryPoint = new EntryPoint{salt: 0}();
         console.log('Deployed entry point at', address(_entryPoint));
