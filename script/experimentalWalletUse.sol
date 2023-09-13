4cf27cc31e6451d108d7628deed9d2999da6a793
Ramon Recuero <rrmoreno1987@gmail.com>
2023-09-13 15:50:03 +0900

    console log

--- script/experimentalWalletUse.sol
+++ script/experimentalWalletUse.sol
@@ -26,10 +26,14 @@ contract ExperimentalWalletUse is Script, UserOp {
         _paymaster = SponsorPaymaster(address(0xef81934A2B6edDB02F4E7C641A8CF2A206c7A513));
         _ethpriceisright = ETHPriceIsRight(address(0xD6b17c41ffd64bB4B922Cbc26b41e3E3AEE4f806));
         _entryPoint = EntryPoint(payable(0x7aD823A5cA21768a3D3041118Bc6e981B0e4D5ee));
+        _walletFactory = KintoWalletFactory(address(0x5d7715A10d22Dc04400eb47D8f776a72b39F9716));
         uint startingNonce = _kintoWalletv1.getNonce();
         console.log('startingNonce', startingNonce);
-
-        uint256 adminPrivateKey = vm.envUint("PRIVATE_KEY");
+        console.log('kinto wallet entryPoint', address(_kintoWalletv1.entryPoint()));
+        console.log('kinto wallet factory wallets', _walletFactory.totalWallets());
+        console.log('eth price guesses', _ethpriceisright.avgGuess());
+        console.log('paymaster balance guesses', _paymaster.balances(address(_ethpriceisright)));
+        uint256 adminPrivateKey = vm.envUint('PRIVATE_KEY');
         vm.startBroadcast(adminPrivateKey);
         
         uint256[] memory privateKeys = new uint256[](1);
@@ -38,10 +42,13 @@ contract ExperimentalWalletUse is Script, UserOp {
         UserOperation memory userOp = this.createUserOperationWithPaymaster(
             address(_kintoWalletv1), startingNonce, privateKeys, address(_ethpriceisright), 0,
             abi.encodeWithSignature('enterGuess(uint)', 5000), address(_paymaster));
+        _ethpriceisright.enterGuess(7000);
+        console.log('eth price guesses 2', _ethpriceisright.avgGuess());
         UserOperation[] memory userOps = new UserOperation[](1);
         userOps[0] = userOp;
 
         _entryPoint.handleOps(userOps, payable(0x1dBDF0936dF26Ba3D7e4bAA6297da9FE2d2428c2));
+        console.log('eth price guesses 3', _ethpriceisright.avgGuess());
 
         vm.stopBroadcast();
     }
