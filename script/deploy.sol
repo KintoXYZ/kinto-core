b98b84648006cb34ffa709a51058cce05033e0d0
Ramon Recuero <rrmoreno1987@gmail.com>
2023-09-18 11:29:22 +0900

    Deploy script

--- script/deploy.sol
+++ script/deploy.sol
@@ -6,6 +6,7 @@ import '../src/KintoID.sol';
 import '../src/interfaces/IKintoID.sol';
 import '../src/interfaces/IKintoWallet.sol';
 import '../src/wallet/KintoWalletFactory.sol';
+import '../src/paymasters/SponsorPaymaster.sol';
 import '@aa/core/EntryPoint.sol';
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol';
 import { SignatureChecker } from '@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol';
@@ -80,25 +81,95 @@ contract KintoAAInitialDeployScript is Script {
     using ECDSAUpgradeable for bytes32;
     using SignatureChecker for address;
 
+    /* solhint-disable var-name-mixedcase */
+    /* solhint-disable private-vars-leading-underscore */
+    address CREATE2_DEPLOYER = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
     KintoWalletFactory _walletFactory;
     EntryPoint _entryPoint;
+    SponsorPaymaster _sponsorPaymaster;
 
     IKintoWallet _kintoWalletv1;
+
+    /// @notice Precompute a contract address deployed via CREATE2
+    function computeAddress(uint256 salt, bytes memory creationCode) public view returns (address) {
+
+        return address(
+            uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), CREATE2_DEPLOYER, salt, keccak256(creationCode)))))
+        );
+    }
+
+    function isContract(address _addr) private view returns (bool){
+        uint32 size;
+        assembly {
+            size := extcodesize(_addr)
+        }
+        return (size > 0);
+    }
 
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
         vm.startBroadcast(deployerPrivateKey);
-        // Create Entry Point
-        _entryPoint = new EntryPoint{salt: 0}();
-        console.log('Entry point deployed at', address(_entryPoint));
-        //Deploy wallet factory
-        _walletFactory = new KintoWalletFactory{salt: 0}(_entryPoint, IKintoID(vm.envAddress('ID_PROXY_ADDRESS')));
-        console.log('Wallet factory deployed at', address(_walletFactory));
-        // Set Wallet Factory in entry point
-        _entryPoint.setWalletFactory(address(_walletFactory));
-        console.log('Wallet factory set in entry point', _entryPoint.walletFactory());
+        bytes memory creationCode = abi.encodePacked(type(EntryPoint).creationCode);
+        address entryPointAddr = address(
+            uint160(uint256(keccak256(abi.encodePacked(
+            bytes1(0xff),
+                CREATE2_DEPLOYER,
+                uint256(0),
+                keccak256(creationCode)
+            )
+            )))
+        );
+
+        console.log('CREATE2 entrypoint addr', entryPointAddr);
+        // Check Entry Point
+        if (isContract(entryPointAddr)) {
+            _entryPoint = EntryPoint(payable(entryPointAddr));
+            console.log('Already deployed at', address(_entryPoint));
+        } else {
+            // Deploy Entry point
+            _entryPoint = new EntryPoint{salt: 0}();
+            console.log('Entry point deployed at', address(_entryPoint));
+        }
+
+        // Check Wallet Factory
+        address walletFactory = EntryPoint(payable(entryPointAddr)).walletFactory();
+        console.log('walletFactory', walletFactory);
+        if (walletFactory == address(0)) {
+            //Deploy wallet factory
+            _walletFactory = new KintoWalletFactory{salt: 0}(_entryPoint, IKintoID(vm.envAddress('ID_PROXY_ADDRESS')));
+            console.log('Wallet factory deployed at', address(_walletFactory));
+            // Set Wallet Factory in entry point
+            _entryPoint.setWalletFactory(address(_walletFactory));
+            console.log('Wallet factory set in entry point', _entryPoint.walletFactory());
+        } else {
+            console.log('Wallet factory already deployed and set in entry point', walletFactory);
+        }
+
+        // Sponsor Paymaster
+        bytes memory creationCodePaymaster = abi.encodePacked(
+            type(SponsorPaymaster).creationCode, abi.encode(address(_entryPoint)));
+        address paymasterAddr = address(
+            uint160(uint256(keccak256(abi.encodePacked(
+            bytes1(0xff),
+                CREATE2_DEPLOYER,
+                uint256(0),
+                keccak256(creationCodePaymaster)
+            )
+            )))
+        );
+        console.log('CREATE2 paymaster addr', paymasterAddr);
+        // Check Paymaster
+        if (isContract(paymasterAddr)) {
+            console.log('Paymaster already deployed at', address(paymasterAddr));
+        } else {
+            // Deploy Entry point
+            _sponsorPaymaster = new SponsorPaymaster{salt: 0}(IEntryPoint(address(_entryPoint)));
+            console.log('Sponsor paymaster deployed at', address(_sponsorPaymaster));
+        }
+
         // address deployerPublicKey = vm.envAddress('PUBLIC_KEY');
         //_kintoWalletv1 = _walletFactory.createAccount(deployerPublicKey, 0);
         // console.log('wallet deployed at', address(_kintoWalletv1));
