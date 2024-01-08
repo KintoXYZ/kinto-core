e0748ab47caffe341d36533ba18681466b191b9b
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-01-08 09:10:12 -0300

    test: add fuzzing

--- test/BytesSignature.t.sol
+++ test/BytesSignature.t.sol
@@ -26,6 +26,27 @@ contract ByteSignatureTest is UserOp {
         assertEq(extracted[1], signature2, "The second signature does not match the original signature.");
     }
 
+    function testFuzzExtractMultipleSignatures(uint8 numSignatures) public {
+        bytes memory fullSignature;
+        bytes[] memory originalSignatures = new bytes[](numSignatures);
+
+        // generate random signatures
+        for (uint8 i = 0; i < numSignatures;i++) {
+            bytes memory signature = new bytes(65);
+            for (uint256 j = 0; j < 65; j++) {
+                signature[j] = bytes1(uint8(uint256(keccak256(abi.encodePacked(block.timestamp, i, j))) % 256));
+            }
+            fullSignature = abi.encodePacked(fullSignature, signature);
+            originalSignatures[i] = signature;
+        }
+
+        bytes[] memory extracted = ByteSignature.extractSignatures(fullSignature, numSignatures);
+
+        for (uint8 i = 0; i < numSignatures; i++) {
+            assertEq(extracted[i], originalSignatures[i], "Signature does not match the original signature.");
+        }
+    }
+
     function testExtract_RevertWhen_InvalidLength() public {
         bytes memory invalidSignature = hex"abcd";
         vm.expectRevert("ByteSignature: Invalid signature length");
