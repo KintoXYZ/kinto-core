b98a59f082731a72e235720a9828a870d59c5085
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-01-15 10:15:31 -0300

    chore: do deployments through factory

--- script/migrations/15-faucet_v2.sol
+++ script/migrations/15-faucet_v2.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import "../../src/Faucet.sol";
+import "../../src/wallet/KintoWalletFactory.sol";
 
 import "../../test/helpers/Create2Helper.sol";
 import "../../test/helpers/ArtifactsReader.sol";
@@ -14,7 +15,6 @@ contract KintoMigration15DeployScript is Create2Helper, ArtifactsReader {
     using ECDSAUpgradeable for bytes32;
 
     Faucet _implementation;
-    Faucet _faucet;
     UUPSProxy _proxy;
 
     function setUp() public {}
@@ -25,43 +25,34 @@ contract KintoMigration15DeployScript is Create2Helper, ArtifactsReader {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         console.log("Executing with address", msg.sender);
 
-        address faucetAddr = payable(_getChainDeployment("Faucet"));
-        if (faucetAddr != address(0)) {
-            console.log("Already deployed faucet", faucetAddr);
-            return;
-        }
+        vm.startBroadcast(deployerPrivateKey);
+
+        // make sure faucet proxy is not already deployed
+        address faucetProxy = payable(_getChainDeployment("Faucet"));
+        require(faucetProxy == address(0), "Faucet proxy is already deployed");
 
         // make sure wallet factory is deployed
         address factory = _getChainDeployment("KintoWalletFactory");
         require(factory != address(0), "Need to deploy the wallet factory first");
 
-        // Faucet
-        faucetAddr = computeAddress(0, abi.encodePacked(type(Faucet).creationCode));
-        if (isContract(faucetAddr)) {
-            _implementation = Faucet(payable(faucetAddr));
-            console.log("Already deployed faucet implementation at", address(faucetAddr));
-        } else {
-            // Deploy Faucet implementation
-            vm.broadcast(deployerPrivateKey);
-            _implementation = new Faucet{salt: 0}(factory);
-            console.log("Faucet implementation deployed at", address(_implementation));
-        }
-        address faucetProxyAddr =
-            computeAddress(0, abi.encodePacked(type(UUPSProxy).creationCode, abi.encode(address(_implementation), "")));
-        if (isContract(faucetProxyAddr)) {
-            _proxy = UUPSProxy(payable(faucetProxyAddr));
-            _faucet = Faucet(payable(address(_proxy)));
-            console.log("Already deployed Faucet proxy at", address(faucetProxyAddr));
-        } else {
-            // deploy proxy contract and point it to implementation
-            vm.broadcast(deployerPrivateKey);
-            _proxy = new UUPSProxy{salt: 0}(address(_implementation), "");
-            console.log("Faucet proxy deployed at ", address(_proxy));
+        // use wallet factory to deploy Faucet implementation & proxy
+        KintoWalletFactory walletFactory = KintoWalletFactory(payable(factory));
 
-            // initialize proxy
-            vm.broadcast(deployerPrivateKey);
-            Faucet(payable(address(_proxy))).initialize();
-        }
+        // deploy Faucet implementation
+        bytes memory bytecode = abi.encodePacked(type(Faucet).creationCode, abi.encode(factory));
+        _implementation = Faucet(payable(walletFactory.deployContract(msg.sender, 0, bytecode, bytes32(0))));
+
+        // deploy Faucet proxy
+        bytecode = abi.encodePacked(type(UUPSProxy).creationCode, abi.encode(address(_implementation), ""));
+        _proxy = UUPSProxy(payable(walletFactory.deployContract(msg.sender, 0, bytecode, bytes32(0))));
+
+        // initialize proxy
+        Faucet(payable(address(_proxy))).initialize();
+
+        vm.stopBroadcast();
+
+        // sanity check: faucet has the new claim amount
+        require(Faucet(payable(address(_proxy))).CLAIM_AMOUNT() == 1 ether / 2500, "Claim amount is not correct");
 
         // Writes the addresses to a file
         console.log(string.concat("Faucet-impl: ", vm.toString(address(_implementation))));
