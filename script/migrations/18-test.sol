206399b7f198609992716c755a5b60ff1085bd81
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-01-18 08:51:34 -0300

    tmp

---
+++ script/migrations/18-test.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "../../test/helpers/Create2Helper.sol";
+import "../../test/helpers/ArtifactsReader.sol";
+import "../../test/helpers/UserOp.sol";
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+contract KintoMigration18DeployScript is Create2Helper, ArtifactsReader, UserOp {
+    using ECDSAUpgradeable for bytes32;
+
+    function setUp() public {}
+
+    function run() public {
+        console.log("Chain ID", vm.toString(block.chainid));
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        console.log("Deployer address: ", vm.addr(deployerPrivateKey));
+
+        _execute(deployerPrivateKey);
+    }
+
+    function _execute(uint256 _signerPk) internal {
+        address _from = 0x58Dd6931DC95292F2E78Cf195a7FC30868Be8aFd;
+        uint256 _nonce = IKintoWallet(_from).getNonce(); 
+        bytes memory _encodedData = "0xb61d27f600000000000000000000000058dd6931dc95292f2e78cf195a7fc30868be8afd0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c4ca85f3340000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030000000000000000000000000c1df30b4576a1a94d9528854516d4d425cf9323000000000000000000000000660ad4b5a74130a4796b4d54bc6750ae93c86e6c0000000000000000000000006fe642404b7b23f31251103ca0efb538ad4aec0700000000000000000000000000000000000000000000000000000000";
+
+        uint256[] memory privateKeys = new uint256[](1);
+        privateKeys[0] = _signerPk;
+
+        address walletOwner = IKintoWallet(_from).owners(0);
+        console.log("Owner", walletOwner);
+        
+        UserOperation[] memory userOps = new UserOperation[](1);
+        userOps[0] = UserOperation({
+            sender: _from,
+            nonce: _nonce,
+            initCode: bytes(""),
+            callData: _encodedData,
+            callGasLimit: 0x7a1200, // generate from call simulation
+            verificationGasLimit: 0x38270, // verification gas. will add create2 cost (3200+200*length) if initCode exists
+            preVerificationGas: 0x9c40, // should also cover calldata cost.
+            maxFeePerGas: 0x6dac2c0, // grab from current gas
+            maxPriorityFeePerGas: 0x6dac2c0, // grab from current gas
+            paymasterAndData: abi.encodePacked(_getChainDeployment("Paymaster")),
+            signature: "0x85982573ef949b5d4b0814f963a2c3929459a4f88ceda2dc7b3a5cee417e1d9b75022db618eee034d2047f84c4d246fe677e6e16b9d23c9ff93610ee99462b0b1c"
+        });
+        
+        // abi.encodeCall(KintoWallet.execute, (_target, _value, _bytesOp))
+        // (address _target, uint256 _value, bytes memory _bytesOp) = decodeExecuteData(_encodedData);
+        // userOps[0] = _createUserOperation(
+        //     block.chainid,
+        //     _from,
+        //     _target,
+        //     _value,
+        //     _nonce + 1,
+        //     privateKeys,
+        //     _bytesOp,
+        //     address(_getChainDeployment("Paymaster"))
+        // );
+
+        // [
+        //     {
+        //         "sender": "0x58Dd6931DC95292F2E78Cf195a7FC30868Be8aFd",
+        //         "nonce": "0x0",
+        //         "initCode": "0x",
+        //         "callData": "0xb61d27f600000000000000000000000058dd6931dc95292f2e78cf195a7fc30868be8afd0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c4ca85f3340000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030000000000000000000000000c1df30b4576a1a94d9528854516d4d425cf9323000000000000000000000000660ad4b5a74130a4796b4d54bc6750ae93c86e6c0000000000000000000000006fe642404b7b23f31251103ca0efb538ad4aec0700000000000000000000000000000000000000000000000000000000",
+        //         "paymasterAndData": "0x1842a4eff3efd24c50b63c3cf89cecee245fc2bd",
+        //         "signature": "0x85982573ef949b5d4b0814f963a2c3929459a4f88ceda2dc7b3a5cee417e1d9b75022db618eee034d2047f84c4d246fe677e6e16b9d23c9ff93610ee99462b0b1c",
+        //         "maxFeePerGas": "0x6dac2c0",
+        //         "maxPriorityFeePerGas": "0x6dac2c0",
+        //         "callGasLimit": "0x7a1200",
+        //         "verificationGasLimit": "0x38270",
+        //         "preVerificationGas": "0x9c40"
+        //     },
+        //     "0x2843C269D2a64eCfA63548E8B3Fc0FD23B7F70cb"
+        // ]
+
+
+        // execute op via entry point
+        vm.deal(0x2843C269D2a64eCfA63548E8B3Fc0FD23B7F70cb, 1 ether);
+        vm.prank(walletOwner);
+        IEntryPoint(_getChainDeployment("EntryPoint")).handleOps(userOps, payable(vm.addr(_signerPk)));
+    }
+
+     function decodeExecuteData(bytes memory data) public view returns (address target, uint256 value, bytes memory bytesOp) {
+        // Skip the first 4 bytes (function selector)
+        bytes memory trimmedData = slice(data, 4, data.length - 4);
+        console.logBytes(trimmedData);
+
+        // Decode the data
+        (target, value, bytesOp) = abi.decode(trimmedData, (address, uint256, bytes));
+        console.log("HOLA");
+
+    }
+
+    // Helper function to slice bytes array
+    function slice(bytes memory data, uint256 start, uint256 length) internal view returns (bytes memory) {
+        console.log("HOLA22");
+        bytes memory part = new bytes(length);
+        console.log("HOLA22");
+        for (uint256 i = 0; i < length; i++) {
+            part[i] = data[i + start];
+        }
+        console.log("HOLA22");
+        return part;
+    }
+}
