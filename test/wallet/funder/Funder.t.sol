a54f03999c3dfa3a16a6e7c1ff593ddf09293fca
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-01-27 08:59:50 +0100

    chore: increase coverage

--- test/wallet/funder/Funder.t.sol
+++ test/wallet/funder/Funder.t.sol
@@ -16,24 +16,50 @@ contract FunderTest is SharedSetup {
     }
 
     function testSetFunderWhitelist() public {
-        vm.startPrank(_owner);
         address[] memory funders = new address[](1);
         funders[0] = address(23);
-        uint256 nonce = _kintoWallet.getNonce();
+
         bool[] memory flags = new bool[](1);
         flags[0] = true;
-        UserOperation memory userOp = _createUserOperation(
+
+        UserOperation[] memory userOps = new UserOperation[](1);
+        userOps[0] = _createUserOperation(
             address(_kintoWallet),
             address(_kintoWallet),
-            nonce,
+            _kintoWallet.getNonce(),
             privateKeys,
             abi.encodeWithSignature("setFunderWhitelist(address[],bool[])", funders, flags),
             address(_paymaster)
         );
-        UserOperation[] memory userOps = new UserOperation[](1);
-        userOps[0] = userOp;
+
         _entryPoint.handleOps(userOps, payable(_owner));
         assertEq(_kintoWallet.isFunderWhitelisted(address(23)), true);
-        vm.stopPrank();
+    }
+
+    function testSetFunderWhitelist_RevertWhen_LengthMismatch() public {
+        address[] memory funders = new address[](2);
+        funders[0] = address(23);
+        funders[1] = address(24);
+
+        bool[] memory flags = new bool[](1);
+        flags[0] = true;
+
+        UserOperation[] memory userOps = new UserOperation[](1);
+        userOps[0] = _createUserOperation(
+            address(_kintoWallet),
+            address(_kintoWallet),
+            _kintoWallet.getNonce(),
+            privateKeys,
+            abi.encodeWithSignature("setFunderWhitelist(address[],bool[])", funders, flags),
+            address(_paymaster)
+        );
+
+        vm.expectEmit(true, true, true, false);
+        emit UserOperationRevertReason(
+            _entryPoint.getUserOpHash(userOps[0]), userOps[0].sender, userOps[0].nonce, bytes("")
+        );
+        vm.recordLogs();
+        _entryPoint.handleOps(userOps, payable(_owner));
+        assertRevertReasonEq("KW-sfw: invalid array");
     }
 }
