7d5918ca34b2d711a5d5797b78c0bd034ebe0d62
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-01-29 19:02:39 +0000

    chore: revert migration

--- src/wallet/KintoWallet.sol
+++ src/wallet/KintoWallet.sol
@@ -438,7 +438,7 @@ contract KintoWallet is Initializable, BaseAccount, TokenCallbackHandler, IKinto
 }
 
 // Upgradeable version of KintoWallet
-contract KintoWalletV5 is KintoWallet {
+contract KintoWalletV4 is KintoWallet {
     constructor(IEntryPoint _entryPoint, IKintoID _kintoID, IKintoAppRegistry _appRegistry)
         KintoWallet(_entryPoint, _kintoID, _appRegistry)
     {}
