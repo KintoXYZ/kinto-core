44518cf5d355e87fddec93e4f660219dd0e81861
Ramon Recuero <rrmoreno1987@gmail.com>
2024-02-24 17:24:42 +0900

    run migration 30

--- script/migrations/30-deploy_paymaster_v9.sol
+++ script/migrations/30-deploy_paymaster_v9.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "../../src/paymasters/SponsorPaymaster.sol";
 import "./utils/MigrationHelper.sol";
 
-contract KintoMigration26DeployScript is MigrationHelper {
+contract KintoMigration30DeployScript is MigrationHelper {
     using ECDSAUpgradeable for bytes32;
 
     function run() public override {
