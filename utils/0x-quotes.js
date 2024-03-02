ff9f2890109397a9f7690d948379ca5f42768def
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-03-02 10:08:44 -0700

    chore: remove unnecessary comments from 0x.js

--- utils/0x-quotes.js
+++ utils/0x-quotes.js
@@ -3,6 +3,7 @@ const ethers = require('ethers');
 
 /**
  * Get a quote from 0x-API to sell the WETH we just deposited into the contract.
+ * @dev you can call this script from CLI with e.g `node ./utils/0x-quotes.js 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497 1000000000000000000`
  * @param {*} sellTokenAddr 
  * @param {*} buyTokenAddr 
  * @param {*} sellAmount 
@@ -16,9 +17,6 @@ const getQuote = async (sellTokenAddr, buyTokenAddr, sellAmount) => {
   const sellETH = sellTokenAddr === 0;
   const buyETH = buyTokenAddr === 0;
 
-  // let decimals = 18;
-  // if (!sellETH) decimals = await sellToken.decimals();
-  // amt = ethers.utils.parseEther(amt, decimals);
   console.info(
     ` - Fetch swap quote from 0x-API to sell ${ethers.formatEther(sellAmount)} ${
       sellETH ? "ETH" : await sellToken.symbol()
@@ -51,11 +49,10 @@ const createQueryString = params => {
     .join("&");
 };
 
-const sellTokenAddr = process.argv[2]; // First argument
-const buyTokenAddr = process.argv[3]; // Second argument
-const sellAmount = process.argv[4]; // Third argument
+const sellTokenAddr = process.argv[2];
+const buyTokenAddr = process.argv[3];
+const sellAmount = process.argv[4];
 
-// Now you can call your function with these arguments
 getQuote(sellTokenAddr, buyTokenAddr, sellAmount)
   .then(quote => console.log("\nQuote:", quote))
   .catch(err => console.error("Error getting quote:", err));
