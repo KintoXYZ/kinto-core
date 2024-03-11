5f6eb8c4fc7aacdc8670b7743430091ccd2feff7
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-03-11 14:32:16 +0100

    feat: allow kyc providers as target in sendMoneyToAccount

---
+++ script/migrations/34-deploy_wallet_factory_v10.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "../../src/wallet/KintoWalletFactory.sol";
+import "./utils/MigrationHelper.sol";
+
+contract KintoMigration34DeployScript is MigrationHelper {
+    using ECDSAUpgradeable for bytes32;
+
+    function run() public override {
+        super.run();
+
+        bytes memory bytecode = abi.encodePacked(
+            type(KintoWalletFactory).creationCode, abi.encode(_getChainDeployment("KintoWalletV6-impl"))
+        );
+        _deployImplementationAndUpgrade("KintoWalletFactory", "V10", bytecode);
+    }
+}
