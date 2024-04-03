de1954c821999efc55d003ec7cccb29256859cf9
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-04-03 10:06:04 +0100

    chore: upgrade bridger L2

--- script/migrations/43-upgrade_bridger_l2.sol
+++ script/migrations/43-upgrade_bridger_l2.sol
@@ -13,7 +13,13 @@ contract KintoMigration43DeployScript is MigrationHelper {
 
         bytes memory bytecode =
             abi.encodePacked(type(BridgerL2).creationCode, abi.encode(_getChainDeployment("KintoWalletFactory")));
+        address implementation = _deployImplementation("BridgerL2", "V9", bytecode);
+        console.log("implementation: %s", implementation);
 
-        _deployImplementationAndUpgrade("BridgerL2", "V9", bytecode);
+        address proxy = _getChainDeployment("BridgerL2");
+        console.log("proxy: %s", proxy);
+        _upgradeTo(proxy, implementation, deployerPrivateKey);
+
+        // _deployImplementationAndUpgrade("BridgerL2", "V9", bytecode);
     }
 }
