00a38c7663e523bcf0c2822e0e754576abd5feae
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-04-09 09:50:21 +0100

    chore: fix migration number & codecov.yml

--- script/migrations/47-transfer_inflator_ownership.sol
+++ script/migrations/47-transfer_inflator_ownership.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "../../src/inflators/KintoInflator.sol";
 import "./utils/MigrationHelper.sol";
 
-contract KintoMigration44DeployScript is MigrationHelper {
+contract KintoMigration47DeployScript is MigrationHelper {
     using ECDSAUpgradeable for bytes32;
 
     function run() public override {
