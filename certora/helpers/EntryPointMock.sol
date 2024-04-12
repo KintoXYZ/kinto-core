e87bac61e3c82016142c6965f926253fe47a67ad
Igor Yalovoy <to@ylv.io>
2024-04-12 19:04:13 -0300

    Update Readme and License (#119)
    * Update README.md to simplify deployment instructions and correct Blockscout capitalization
    
    * Update SPDX license identifier from GPL-3.0/UNLICENSED to MIT in multiple Solidity files
    
    * Add MIT License to the project, granting extensive permissions to users and contributors from 2016-2024.
    
    * Update package.json with new homepage, version, and dependencies; simplify test scripts; add newline in 10-upgrade_wallet_v3.sol; remove yarn-error.log
    
    * Remove husky pre-commit hook and its dependency from project.

--- certora/helpers/EntryPointMock.sol
+++ certora/helpers/EntryPointMock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 import { StakeManager } from "lib/account-abstraction/contracts/core/StakeManager.sol";
