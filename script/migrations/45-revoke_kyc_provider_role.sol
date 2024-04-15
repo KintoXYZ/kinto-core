0ab2309d7119bab2ff623bd6782b2cb1eafa77f3
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-04-15 12:49:51 +0100

    chore: migration to revoke old KYC provider

---
+++ script/migrations/45-revoke_kyc_provider_role.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "../../src/KintoID.sol";
+import "./utils/MigrationHelper.sol";
+
+contract KintoMigration45DeployScript is MigrationHelper {
+    using ECDSAUpgradeable for bytes32;
+
+    function run() public override {
+        super.run();
+
+        // revoke KYC_PROVIDER_ROLE from old KYC provider address
+        address oldKYCProvider = 0x6fe642404B7B23F31251103Ca0efb538Ad4aeC07;
+        KintoID kintoID = KintoID(_getChainDeployment("KintoID"));
+        bytes32 role = kintoID.KYC_PROVIDER_ROLE();
+
+        assertTrue(kintoID.hasRole(role, oldKYCProvider));
+
+        vm.broadcast();
+        kintoID.revokeRole(role, oldKYCProvider);
+
+        assertFalse(kintoID.hasRole(role, oldKYCProvider));
+    }
+}
