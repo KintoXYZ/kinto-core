b9af83e33262363766cd0406c05866918b66a1d4
Igor Yalovoy <to@ylv.io>
2024-05-01 16:17:12 -0300

    Remove minimum amount out check from SwapWorkflow (#134)
    * Remove minimum amount out check from SwapWorkflow.sol and associated test case in SwapWorkflow.t.sol
    
    * Refactor fillQuote function signature for clarity in SwapWorkflow.sol

--- test/workflows/SwapWorkflow.t.sol
+++ test/workflows/SwapWorkflow.t.sol
@@ -93,29 +93,6 @@ contract SwapWorkflowTest is UserOp, SharedSetup {
         assertEq(workflow.exchangeProxy(), EXCHANGE_PROXY);
     }
 
-    function testSwap_RevertWhen_AmountOutTooLow() public {
-        if (!fork) vm.skip(true);
-
-        uint256 amountIn = 1e3 * 1e6;
-        uint256 minAmountOut = amountIn * 1e12;
-        uint256 expectedAmountOut = 999842220668049737510;
-        // block number in which the 0x API data was fetched
-        vm.rollFork(19725885);
-        deploy();
-
-        // Run to regenerate
-        // curl 'https://api.0x.org/swap/v1/quote?buyToken=0x6B175474E89094C44Da98b954EedeAC495271d0F&sellToken=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48&sellAmount=1000000000' --header '0x-api-key: KEY' | jq > ./test/data/swap-quote.json
-        string memory quote = vm.readFile("./test/data/swap-quote.json");
-        bytes memory swapCallData = quote.readBytes(".data");
-        bytes memory data =
-            abi.encodeWithSelector(SwapWorkflow.fillQuote.selector, USDC, amountIn, DAI, minAmountOut, swapCallData);
-
-        deal(USDC, address(accessPoint), amountIn);
-        vm.expectRevert(abi.encodeWithSelector(SwapWorkflow.AmountOutTooLow.selector, expectedAmountOut, minAmountOut));
-        vm.prank(_user);
-        accessPoint.execute(address(swapWorkflow), data);
-    }
-
     function testSwapERC20() public {
         if (!fork) vm.skip(true);
 
