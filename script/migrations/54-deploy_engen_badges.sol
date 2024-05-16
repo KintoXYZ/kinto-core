1be26a9db872a628438a36b35da6e666a2d206cb
VSB <v.sanchezbelmar@gmail.com>
2024-05-16 15:01:06 -0700

    Engen badges (#144)
    * feat: EngenBadges ERC1155 NFTs
    
    * chore: change visibility to external when possible
    
    * chore: remove harness, remove initialadmin param
    
    * chore: update IEngenBadges interface
    
    * cleanup
    
    * AA refactor
    
    * chore: linter formatting
    
    * fix
    
    * chore: add coverage for supportInterface
    
    * chore: name, symbol and uri updates

---
+++ script/migrations/54-deploy_engen_badges.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@kinto-core/tokens/EngenBadges.sol";
+import "../../test/helpers/ArtifactsReader.sol";
+import "@kinto-core-script/utils/MigrationHelper.sol";
+
+contract KintoMigration54DeployScript is MigrationHelper {
+    using ECDSAUpgradeable for bytes32;
+
+    function run() public override {
+        super.run();
+
+        //is it already deployed?
+        address engenBadgesAddr = _getChainDeployment("EngenBadges");
+        if (engenBadgesAddr != address(0)) {
+            console.log("EngenBadges already deployed", engenBadgesAddr);
+            return;
+        }
+
+        bytes memory bytecode =
+            abi.encodePacked(type(EngenBadges).creationCode, abi.encode(_getChainDeployment("KintoWalletFactory")));
+        address implementation = _deployImplementation("EngenBadges", "V1", bytecode);
+        address proxy = _deployProxy("EngenBadges", implementation);
+
+        _fundPaymaster(proxy, deployerPrivateKey);
+        _whitelistApp(proxy, deployerPrivateKey);
+
+        //UserOp initialize with parameters
+        _handleOps(
+            abi.encodeWithSelector(EngenBadges.initialize.selector, "https://kinto.xyz/api/v1/get-badge-nft/{id}"),
+            address(proxy),
+            deployerPrivateKey
+        );
+
+        //Remove comment to mint sample badges for the KintoWallet-admin
+
+        /*
+        uint256[] memory ids = new uint256[](2);
+        ids[0] = 0;
+        ids[1] = 3;
+        
+        _handleOps(
+            abi.encodeWithSelector(
+                EngenBadges.mintBadges.selector,
+                _getChainDeployment("KintoWallet-admin"),
+                ids
+            ), 
+            address(proxy),
+            deployerPrivateKey
+        );
+        */
+    }
+}
