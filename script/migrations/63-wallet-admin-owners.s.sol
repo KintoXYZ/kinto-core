19f1a8c412c973dd3368429f08e5bb827f3d00f8
VictorSB <v.sanchezbelmar@gmail.com>
2024-05-21 15:08:35 -0700

    fix format for signers

--- script/migrations/63-wallet-admin-owners.s.sol
+++ script/migrations/63-wallet-admin-owners.s.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.18;
 import "@kinto-core-script/utils/MigrationHelper.sol";
 import "@kinto-core/interfaces/IKintoWallet.sol";
 
-
 contract KintoMigration63DeployScript is MigrationHelper {
     using ECDSAUpgradeable for bytes32;
 
@@ -13,11 +12,10 @@ contract KintoMigration63DeployScript is MigrationHelper {
 
         address _walletAdmin = _getChainDeployment("KintoWallet-admin");
 
-        address[3] memory signers = [
-            0x660ad4B5A74130a4796B4d54BC6750Ae93C86e6c,
-            0xc1f4D15C16A1f3555E0a5F7AeFD1e17AD4aaf40B,
-            0x94561e98DD5E55271f91A103e4979aa6C493745E
-        ];
+        address[] memory signers = new address[](3);
+        signers[0] = 0x660ad4B5A74130a4796B4d54BC6750Ae93C86e6c;
+        signers[1] = 0xc1f4D15C16A1f3555E0a5F7AeFD1e17AD4aaf40B;
+        signers[2] = 0x94561e98DD5E55271f91A103e4979aa6C493745E;
 
         bytes memory selectorAndParams = abi.encodeWithSelector(IKintoWallet.resetSigners.selector, signers, 2);
         _handleOps(selectorAndParams, _walletAdmin, deployerPrivateKey);
