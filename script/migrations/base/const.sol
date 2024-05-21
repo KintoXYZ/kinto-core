e0c17646e75f895c8993d06575ba57d43ddd7abd
Igor Yalovoy <to@ylv.io>
2024-05-21 16:00:05 -0600

    Add Bridger deployment script, constants, and update addresses for chain 8453.

---
+++ script/migrations/base/const.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Constants {
+    address internal constant L2_VAULT = 0x26181Dfc530d96523350e895180b09BAf3d816a0;
+    address internal constant EXCHANGE_PROXY = 0xDef1C0ded9bec7F1a1670819833240f027b25EfF;
+    address internal constant WETH = 0x4200000000000000000000000000000000000006;
+    address internal constant SENDER_ACCOUNT = 0x6E09F8A68fB5278e0C33D239dC12B2Cec33F4aC7;
+    uint256 internal constant BASE_CHAINID = 8453;
+}
