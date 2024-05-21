4eef09c7d3c6e15b1f7b03ec1dfb310bed17f1a6
VictorSB <v.sanchezbelmar@gmail.com>
2024-05-21 15:00:21 -0700

    chore: add owners

---
+++ script/migrations/63-wallet-admin-owners.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@kinto-core-script/utils/MigrationHelper.sol";
+import "@kinto-core/interfaces/IKintoWallet.sol";
+
+
+contract KintoMigration63DeployScript is MigrationHelper {
+    using ECDSAUpgradeable for bytes32;
+
+    function run() public override {
+        super.run();
+
+        address _walletAdmin = _getChainDeployment("KintoWallet-admin");
+
+        address[3] memory signers = [
+            0x660ad4B5A74130a4796B4d54BC6750Ae93C86e6c,
+            0xc1f4D15C16A1f3555E0a5F7AeFD1e17AD4aaf40B,
+            0x94561e98DD5E55271f91A103e4979aa6C493745E
+        ];
+
+        bytes memory selectorAndParams = abi.encodeWithSelector(IKintoWallet.resetSigners.selector, signers, 2);
+        _handleOps(selectorAndParams, _walletAdmin, deployerPrivateKey);
+    }
+}
