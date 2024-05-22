28bc80b0c03be42d99584fb06ed3a91714063e26
Ramon Recuero <rrmoreno1987@gmail.com>
2024-05-21 19:29:47 -0700

    Bridger fix (#162)
    * Bridger fix
    
    * Upgrades bridger

--- src/socket/IConnector.sol
+++ src/socket/IConnector.sol
@@ -2,18 +2,14 @@
 pragma solidity ^0.8.13;
 
 interface IConnector {
-    function outbound(
-        uint256 msgGasLimit_,
-        bytes memory payload_,
-        bytes memory options_
-    ) external payable returns (bytes32 messageId_);
+    function outbound(uint256 msgGasLimit_, bytes memory payload_, bytes memory options_)
+        external
+        payable
+        returns (bytes32 messageId_);
 
     function siblingChainSlug() external view returns (uint32);
 
-    function getMinFees(
-        uint256 msgGasLimit_,
-        uint256 payloadSize_
-    ) external view returns (uint256 totalFees);
+    function getMinFees(uint256 msgGasLimit_, uint256 payloadSize_) external view returns (uint256 totalFees);
 
     function getMessageId() external view returns (bytes32);
 }
--- src/socket/IController.sol
+++ src/socket/IController.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.13;
 
 interface IController {
-    function identifierCache(
-        bytes32 messageId_
-    ) external payable returns (bytes memory cache);
+    function identifierCache(bytes32 messageId_) external payable returns (bytes memory cache);
 
-    function connectorCache(
-        address connector_
-    ) external payable returns (bytes memory cache);
+    function connectorCache(address connector_) external payable returns (bytes memory cache);
 }
