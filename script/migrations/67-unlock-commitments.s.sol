dc48b35c18419f8725dc0dc2a1e02d539f0c2c90
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-05-22 18:09:04 +0100

    Unlock commitments

---
+++ script/migrations/67-unlock-commitments.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../../src/wallet/KintoWallet.sol";
+import "@kinto-core-script/utils/MigrationHelper.sol";
+import "@kinto-core/bridger/BridgerL2.sol";
+
+contract KintoMigration67DeployScript is MigrationHelper {
+    using ECDSAUpgradeable for bytes32;
+
+    function run() public override {
+        super.run();
+        KintoWallet kintoWallet = KintoWallet(payable(_getChainDeployment("KintoWallet-admin")));
+        bytes memory selectorAndParams = abi.encodeWithSelector(BridgerL2.unlockCommitments.selector);
+        _handleOps(selectorAndParams, address(kintoWallet), deployerPrivateKey, "ledger");
+    }
+}
