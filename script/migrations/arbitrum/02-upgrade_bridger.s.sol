b71028161e7d35aa24e392307322a220a3088039
Igor Yalovoy <to@ylv.io>
2024-06-12 09:56:15 -0600

    Refactor UpgradeBridgerScript to initialize Bridger instance before new implementation and update sender account assertion.

--- script/migrations/arbitrum/02-upgrade_bridger.s.sol
+++ script/migrations/arbitrum/02-upgrade_bridger.s.sol
@@ -19,13 +19,13 @@ contract UpgradeBridgerScript is Constants, Test, MigrationHelper {
     function run() public override {
         super.run();
 
-        address newImpl = new Bridger(EXCHANGE_PROXY, CURVE_USDM_POOL, USDC, WETH, address(0), address(0), address(0), address(0));
+        Bridger bridger = Bridger(payable(_getChainDeployment("Bridger")));
+
+        address newImpl = address(new Bridger(EXCHANGE_PROXY, CURVE_USDM_POOL, USDC, WETH, address(0), address(0), address(0), address(0)));
         bridger.upgradeTo(address(newImpl));
 
         // Checks
-        Bridger bridger = Bridger(payable(_getChainDeployment("Bridger")));
-
-        assertEq(bridger.senderAccount(), SENDER_ACCOUNT, "Invalid Sender Account");
+        assertEq(bridger.senderAccount(), 0x89A01e3B2C3A16c3960EADc2ceFcCf2D3AA3F82e, "Invalid Sender Account");
         assertEq(bridger.usdmCurvePool(), CURVE_USDM_POOL, "Invalid USDM Curve Pool");
         assertEq(bridger.owner(), deployer, "Invalid Owner");
         console.log("BridgerV3-impl at: %s", address(newImpl));
