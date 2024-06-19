ff795c441dc49687aa8ea37dc6bcf03309f71582
Igor Yalovoy <to@ylv.io>
2024-06-19 08:52:36 -0600

    Update Merkle tree data, add new tests for RewardsDistributor, and modify build script path.

--- test/unit/RewardsDistributor.t.sol
+++ test/unit/RewardsDistributor.t.sol
@@ -8,6 +8,7 @@ import {stdJson} from "forge-std/StdJson.sol";
 
 import {IERC20} from "@openzeppelin-5.0.1/contracts/token/ERC20/IERC20.sol";
 import {ECDSA} from "@openzeppelin-5.0.1/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin-5.0.1/contracts/access/Ownable.sol";
 
 import {ForkTest} from "@kinto-core-test/helpers/ForkTest.sol";
 import {ERC20Mock} from "@kinto-core-test/helpers/ERC20Mock.sol";
@@ -17,7 +18,7 @@ import {RewardsDistributor} from "@kinto-core/RewardsDistributor.sol";
 contract RewardsDistributorTest is ForkTest {
     RewardsDistributor internal distributor;
     ERC20Mock internal kinto;
-    bytes32 internal root = 0xf5d3a04b6083ba8077d903785b3001db5b9077f1a3af3e06d27a8a9fa3567546;
+    bytes32 internal root = 0x4f75b6d95fab3aedde221f8f5020583b4752cbf6a155ab4e5405fe92881f80e6;
     bytes32 internal leaf;
     uint256 internal baseAmount = 600_000e18;
     uint256 internal maxRatePerSecond = 3e16; // 0.03 tokens per second, or 2,592 tokens per day
@@ -53,8 +54,9 @@ contract RewardsDistributorTest is ForkTest {
         assertEq(kinto.balanceOf(address(distributor)), amount);
         assertEq(kinto.balanceOf(_user), 0);
 
-        bytes32[] memory proof = new bytes32[](1);
+        bytes32[] memory proof = new bytes32[](2);
         proof[0] = 0xb92c48e9d7abe27fd8dfd6b5dfdbfb1c9a463f80c712b66f3a5180a090cccafc;
+        proof[1] = 0xfe69d275d3541c8c5338701e9b211e3fc949b5efb1d00a410313e7474952967f;
 
         distributor.claim(proof, _user, amount);
 
@@ -62,6 +64,44 @@ contract RewardsDistributorTest is ForkTest {
         assertEq(kinto.balanceOf(_user), amount);
         assertEq(distributor.totalClaimed(), amount);
         assertEq(distributor.claimedByUser(_user), amount);
+        assertEq(distributor.getTotalLimit(), baseAmount);
+        assertEq(distributor.getUnclaimedLimit(), baseAmount - amount);
+    }
+
+    function testClaimMultiple() public {
+        uint256 amount = 1e18;
+
+        kinto.mint(address(distributor), amount);
+
+        assertEq(kinto.balanceOf(address(distributor)), amount);
+        assertEq(kinto.balanceOf(_user), 0);
+
+        bytes32[] memory proof = new bytes32[](2);
+        proof[0] = 0xb92c48e9d7abe27fd8dfd6b5dfdbfb1c9a463f80c712b66f3a5180a090cccafc;
+        proof[1] = 0xfe69d275d3541c8c5338701e9b211e3fc949b5efb1d00a410313e7474952967f;
+
+        distributor.claim(proof, _user, amount);
+
+        assertEq(kinto.balanceOf(address(distributor)), 0);
+        assertEq(kinto.balanceOf(_user), amount);
+        assertEq(distributor.totalClaimed(), amount);
+        assertEq(distributor.claimedByUser(_user), amount);
+        assertEq(distributor.getTotalLimit(), baseAmount);
+        assertEq(distributor.getUnclaimedLimit(), baseAmount - amount);
+
+        kinto.mint(address(distributor), amount);
+
+        proof[0] = 0xf99b282683659c94d424bb86cf2a97562a08a76b5aee76ae401a001c75ca8f02;
+        proof[1] = 0xf5d3a04b6083ba8077d903785b3001db5b9077f1a3af3e06d27a8a9fa3567546;
+
+        distributor.claim(proof, _user, amount);
+
+        assertEq(kinto.balanceOf(address(distributor)), 0);
+        assertEq(kinto.balanceOf(_user), 2 * amount);
+        assertEq(distributor.totalClaimed(), 2 * amount);
+        assertEq(distributor.claimedByUser(_user), 2 * amount);
+        assertEq(distributor.getTotalLimit(), baseAmount);
+        assertEq(distributor.getUnclaimedLimit(), baseAmount - 2 * amount);
     }
 
     function testClaim_RevertWhen_InvalidProof() public {
@@ -79,9 +119,59 @@ contract RewardsDistributorTest is ForkTest {
         distributor = new RewardsDistributor(kinto, root, 0, maxRatePerSecond, startTime);
         uint256 amount = 1e18;
 
-        bytes32[] memory proof = new bytes32[](1);
+        bytes32[] memory proof = new bytes32[](2);
         proof[0] = 0xb92c48e9d7abe27fd8dfd6b5dfdbfb1c9a463f80c712b66f3a5180a090cccafc;
+        proof[1] = 0xfe69d275d3541c8c5338701e9b211e3fc949b5efb1d00a410313e7474952967f;
+
         vm.expectRevert(abi.encodeWithSelector(RewardsDistributor.MaxLimitReached.selector, amount, 0));
         distributor.claim(proof, _user, amount);
+    }
+
+    function testUpdateRoot() public {
+        bytes32 newRoot = 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef;
+
+        vm.prank(_owner);
+        distributor.updateRoot(newRoot);
+
+        assertEq(distributor.root(), newRoot);
+    }
+
+    function testUpdateRoot_RevertWhen_NotOwner() public {
+        bytes32 newRoot = 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef;
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
+        distributor.updateRoot(newRoot);
+    }
+
+    function testUpdateBaseAmount() public {
+        uint256 newBaseAmount = 1_000_000e18;
+
+        vm.prank(_owner);
+        distributor.updateBaseAmount(newBaseAmount);
+
+        assertEq(distributor.baseAmount(), newBaseAmount);
+    }
+
+    function testUpdateBaseAmount_RevertWhen_NotOwner() public {
+        uint256 newBaseAmount = 1_000_000e18;
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
+        distributor.updateBaseAmount(newBaseAmount);
+    }
+
+    function testUpdateMaxRatePerSecond() public {
+        uint256 newMaxRatePerSecond = 5e16; // 0.05 tokens per second
+
+        vm.prank(_owner);
+        distributor.updateMaxRatePerSecond(newMaxRatePerSecond);
+
+        assertEq(distributor.maxRatePerSecond(), newMaxRatePerSecond);
+    }
+
+    function testUpdateMaxRatePerSecond_RevertWhen_NotOwner() public {
+        uint256 newMaxRatePerSecond = 5e16; // 0.05 tokens per second
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(this)));
+        distributor.updateMaxRatePerSecond(newMaxRatePerSecond);
     }
 }
--- utils/build-merkle-tree.mjs
+++ utils/build-merkle-tree.mjs
@@ -4,6 +4,8 @@ import fs from "fs";
 // (1)
 const values = [
   ["0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69", "1000000000000000000"],
+  ["0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69", "1000000000000000000"],
+  ["0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69", "1000000000000000000"],
   ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
 ];
 
@@ -14,7 +16,7 @@ const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
 console.log('Merkle Root:', tree.root);
 
 // (4)
-fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+fs.writeFileSync("./test/data/rd-merkle-tree.json", JSON.stringify(tree.dump()));
 
 for (const [i, v] of tree.entries()) {
     const proof = tree.getProof(i);
