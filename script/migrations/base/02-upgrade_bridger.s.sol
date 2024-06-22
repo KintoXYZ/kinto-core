40dfd75356203afad5206ed0a55e400e766bb6c6
Igor Yalovoy <to@ylv.io>
2024-06-22 14:52:30 -0600

    Added BridgerV2 implementation, updated migration script, and modified constants and addresses for Base chain.

---
+++ script/migrations/base/02-upgrade_bridger.s.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import {Bridger} from "@kinto-core/bridger/Bridger.sol";
+
+import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
+import {ArtifactsReader} from "@kinto-core-test/helpers/ArtifactsReader.sol";
+import {UUPSProxy} from "@kinto-core-test/helpers/UUPSProxy.sol";
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+
+import {Constants} from "@kinto-core-script/migrations/base/const.sol";
+
+contract UpgradeBridgerScript is Constants, Test, MigrationHelper {
+    function run() public override {
+        super.run();
+
+        if (block.chainid != BASE_CHAINID) {
+            console2.log("This script is meant to be run on the chain: %s", BASE_CHAINID);
+            return;
+        }
+
+        Bridger bridger = Bridger(payable(_getChainDeployment("Bridger")));
+
+        // Set DAI to zero, as it has a normal `permit` on Base.
+        // Set wstEth to zero, as staking is not supported on Base.
+        // Set USDe and sUSDe to zero, as staking USDe is not supported on Base.
+        // Set Curve pool and USDC to zero as we do not support USDM on Base.
+        vm.broadcast(deployerPrivateKey);
+        address newImpl = address(
+            new Bridger(EXCHANGE_PROXY, address(0), address(0), WETH, address(0), address(0), address(0), address(0))
+        );
+
+        vm.prank(bridger.owner());
+        bridger.upgradeTo(address(newImpl));
+
+        // Checks
+        assertEq(bridger.senderAccount(), SENDER_ACCOUNT, "Invalid Sender Account");
+        assertEq(bridger.owner(), deployer, "Invalid Owner");
+        saveContractAddress('BridgerV2-impl', newImpl);
+    }
+}
