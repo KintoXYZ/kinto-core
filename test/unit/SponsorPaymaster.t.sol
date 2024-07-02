dcff75a6a3c8078c953d7e749f14ebe76c539c9d
Igor Yalovoy <to@ylv.io>
2024-07-02 16:39:45 -0600

    Refactored appLimits array initialization to a single line in SponsorPaymasterTest contract for better readability.

--- test/unit/SponsorPaymaster.t.sol
+++ test/unit/SponsorPaymaster.t.sol
@@ -541,12 +541,7 @@ contract SponsorPaymasterTest is SharedSetup {
         /// fixme: once _setOperationCount works fine, refactor and use _setOperationCount;
         /// @dev create app with high app limits and low gas limit so we assert that the one used
         // in the test is the gas limit
-        uint256[4] memory appLimits = [
-            100e18,
-            100e18,
-            _kintoAppRegistry.GAS_LIMIT_PERIOD(),
-            1 wei
-        ];
+        uint256[4] memory appLimits = [100e18, 100e18, _kintoAppRegistry.GAS_LIMIT_PERIOD(), 1 wei];
         updateMetadata(_owner, "counter", address(counter), appLimits, new address[](0));
 
         // execute transactions (with one user op per tx) one by one until reaching the gas limit
@@ -565,12 +560,7 @@ contract SponsorPaymasterTest is SharedSetup {
         /// fixme: once _setOperationCount works fine, refactor and use _setOperationCount;
         /// @dev create app with high app limits and low gas limit so we assert that the one used
         // in the test is the gas limit
-        uint256[4] memory appLimits = [
-            100e18,
-            100e18,
-            _kintoAppRegistry.GAS_LIMIT_PERIOD(),
-            1 wei
-        ];
+        uint256[4] memory appLimits = [100e18, 100e18, _kintoAppRegistry.GAS_LIMIT_PERIOD(), 1 wei];
         updateMetadata(_owner, "counter", address(counter), appLimits, new address[](0));
 
         // execute transactions until reaching gas limit and save the amount of apps that reached the threshold
