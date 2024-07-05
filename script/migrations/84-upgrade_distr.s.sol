74e22e1d24ec942f55aa9dc114d13b5941dd9e85
Igor Yalovoy <to@ylv.io>
2024-07-05 14:17:02 -0600

    Commented out old implementation deployment and directly upgraded RewardsDistributor to new implementation address in 84-upgrade_distr.s.sol.

--- script/migrations/84-upgrade_distr.s.sol
+++ script/migrations/84-upgrade_distr.s.sol
@@ -19,14 +19,16 @@ contract UpgradeDistributorScript is MigrationHelper {
         // replaceOwner(IKintoWallet(kintoAdminWallet), 0x4632F4120DC68F225e7d24d973Ee57478389e9Fd);
         // hardwareWalletType = 1;
 
-        address impl = _deployImplementationAndUpgrade("RewardsDistributor", "V3", bytecode, keccak256("V3"));
+        //address impl = _deployImplementationAndUpgrade("RewardsDistributor", "V3", bytecode, keccak256("V3"));
 
         RewardsDistributor distr = RewardsDistributor(_getChainDeployment("RewardsDistributor"));
+
+        _upgradeTo(address(distr), 0x86FCC650cE6FfA75ee1dfF98d943e4d1ff16c4fB, deployerPrivateKey);
 
         assertEq(address(distr.KINTO()), 0x010700808D59d2bb92257fCafACfe8e5bFF7aB87);
         assertEq(address(distr.ENGEN()), 0xD1295F0d8789c3E0931A04F91049dB33549E9C8F);
         assertEq(distr.startTime(), LIQUIDITY_MINING_START_DATE);
 
-        saveContractAddress("RewardsDistributorV3-impl", impl);
+        saveContractAddress("RewardsDistributorV3-impl", 0x86FCC650cE6FfA75ee1dfF98d943e4d1ff16c4fB);
     }
 }
