a20b2a78dd3615023c630e54bfe689386249edf1
Igor Yalovoy <to@ylv.io>
2024-07-09 12:22:57 -0300

    Transfer K tokens to DCLM (#227)
    * Add TransferDclmScript for migrating and handling token transfers in the Kinto ecosystem.
    
    * Added HARDWARE_WALLET_TYPE to .env.sample, created new JSON files for 89-transfer-dclm.s.sol, removed commented code in TransferDclmScript, and updated MigrationHelper to set hardwareWalletType from environment variable.

---
+++ script/migrations/89-transfer-dclm.s.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {LibString} from "solady/utils/LibString.sol";
+import {ERC20} from "@openzeppelin-5.0.1/contracts/token/ERC20/ERC20.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {BridgedToken} from "@kinto-core/tokens/bridged/BridgedToken.sol";
+import {BridgedKinto} from "@kinto-core/tokens/bridged/BridgedKinto.sol";
+import {IKintoWallet} from "@kinto-core/interfaces/IKintoWallet.sol";
+import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
+import {UUPSProxy} from "@kinto-core-test/helpers/UUPSProxy.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract TransferDclmScript is MigrationHelper {
+    using LibString for *;
+    using Strings for string;
+    using stdJson for string;
+
+    function run() public override {
+        super.run();
+
+        uint256 amount = 85_000e18;
+        address DCLM = 0x26E508D5d63499e549D958B42c4e2630272Ce2a2;
+
+        address kintoToken = _getChainDeployment("KINTO");
+
+        // Burn tokens from RD
+        _handleOps(
+            abi.encodeWithSelector(BridgedToken.burn.selector, _getChainDeployment("RewardsDistributor"), amount),
+            kintoToken
+        );
+
+        // Mint tokens to DCLM address
+        _handleOps(abi.encodeWithSelector(BridgedToken.mint.selector, DCLM, amount), kintoToken);
+
+        // Check that tokens received
+        assertEq(ERC20(kintoToken).balanceOf(DCLM), amount);
+    }
+}
