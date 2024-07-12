59e0b7769970daa76a33094fe0e1682acff7743f
Igor Yalovoy <to@ylv.io>
2024-07-12 09:03:32 -0600

    Refactored KintoAppRegistry.sol to enhance readability, added detailed comments, and updated import statements for better modularity.

--- src/apps/KintoAppRegistry.sol
+++ src/apps/KintoAppRegistry.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import {ERC721EnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import "../interfaces/IKintoID.sol";
-import "../interfaces/IKintoAppRegistry.sol";
-import "../interfaces/IKintoWalletFactory.sol";
-import "../interfaces/IKintoWallet.sol";
+import {KintoWallet} from "@kinto-core/wallet/KintoWallet.sol";
+import {IKintoID} from "@kinto-core/interfaces/IKintoID.sol";
+import {IKintoAppRegistry} from "@kinto-core/interfaces/IKintoAppRegistry.sol";
+import {IKintoWalletFactory} from "@kinto-core/interfaces/IKintoWalletFactory.sol";
+import {IKintoWallet} from "@kinto-core/interfaces/IKintoWallet.sol";
 
 /**
  * @title KintoAppRegistry
- * @dev A contract that holds all the information of a KintoApp
+ * @notice A contract for managing the registration and metadata of KintoApps
+ * @dev This contract handles the following main functionalities:
+ * 1. Registration of new KintoApps
+ * 2. Updating app metadata
+ * 3. Managing sponsored contracts
+ * 4. Enabling Data Sharing Agreement (DSA) for apps
+ * 5. Managing system contracts
+ * 6. Controlling access for Externally Owned Accounts (EOAs)
+ *
+ * The contract uses ERC721 for representing apps as unique tokens and implements
+ * upgradeability using the UUPS pattern. It also interacts with KintoID for KYC
+ * verification and KintoWalletFactory for wallet-related operations.
  */
 contract KintoAppRegistry is
     Initializable,
@@ -26,39 +38,57 @@ contract KintoAppRegistry is
 {
     /* ============ Constants ============ */
 
+    /// @notice The default period for rate limiting, set to 1 minute
     uint256 public constant override RATE_LIMIT_PERIOD = 1 minutes;
+
+    /// @notice The default threshold for rate limiting, set to 10 calls
     uint256 public constant override RATE_LIMIT_THRESHOLD = 10;
+
+    /// @notice The default period for gas limiting, set to 30 days
     uint256 public constant override GAS_LIMIT_PERIOD = 30 days;
+
+    /// @notice The default threshold for gas limiting, set to 0.01 ether
     uint256 public constant override GAS_LIMIT_THRESHOLD = 0.01 ether;
 
+    /// @notice The address of the CREATE2 contract
     address public constant CREATE2 = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
 
+    /// @notice The KintoWalletFactory contract
     IKintoWalletFactory public immutable override walletFactory;
 
+    /// @notice The KintoID contract
     IKintoID public immutable kintoID;
 
+    /// @notice The address of the admin deployer
     address public constant ADMIN_DEPLOYER = 0x660ad4B5A74130a4796B4d54BC6750Ae93C86e6c;
 
     /* ============ State Variables ============ */
 
+    /// @notice Mapping of app addresses to their metadata
     mapping(address => IKintoAppRegistry.Metadata) private _appMetadata;
 
-    // mapping between an app and all the contracts that it sponsors (that belong to the app)
-    mapping(address => address) public override childToParentContract; // child => parent (app)
+    /// @notice Mapping of child contracts to their parent app contracts
+    mapping(address => address) public override childToParentContract;
 
-    // contracts the app decides to sponsor (that dont belong to the app)
+    /// @notice Mapping of apps to their sponsored contracts
     mapping(address => mapping(address => bool)) private _sponsoredContracts;
 
-    mapping(uint256 => address) public override tokenIdToApp; // token ID => app metadata
+    /// @notice Mapping of token IDs to their corresponding app addresses
+    mapping(uint256 => address) public override tokenIdToApp;
 
+    /// @notice The total number of registered apps
     uint256 public override appCount;
 
+    /// @notice Mapping of developer EOAs to their associated app addresses
     mapping(address => address) public override devEoaToApp;
 
+    /// @notice Array of system contract addresses
     address[] public override systemContracts;
 
+    /// @notice Mapping to check if an address is a system contract
     mapping(address => bool) public override isSystemContract;
 
+    /// @notice Mapping of deployer EOAs to their associated wallet addresses
     mapping(address => address) public override deployerToWallet;
 
     /* ============ Events ============ */
@@ -71,13 +101,18 @@ contract KintoAppRegistry is
 
     /* ============ Constructor & Initializers ============ */
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    /**
+     * @notice Constructs the KintoAppRegistry contract
+     * @param _walletFactory The address of the KintoWalletFactory contract
+     * @custom:oz-upgrades-unsafe-allow constructor
+     */
     constructor(IKintoWalletFactory _walletFactory) {
         _disableInitializers();
         walletFactory = _walletFactory;
         kintoID = IKintoID(_walletFactory.kintoID());
     }
 
+    /// @notice Initializes the contract
     function initialize() external initializer {
         __ERC721_init("Kinto APP", "KINTOAPP");
         __ERC721Enumerable_init();
@@ -87,33 +122,32 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Authorize the upgrade. Only by the upgrader role.
-     * @param newImplementation address of the new implementation
+     * @notice Authorizes the upgrade of the contract
+     * @param newImplementation The address of the new implementation
      */
-    // This function is called by the proxy contract when the implementation is upgraded
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     /* ============ Token name, symbol & URI ============ */
 
     /**
-     * @dev Gets the token name.
-     * @return string representing the token name
+     * @notice Gets the token name
+     * @return The name of the token
      */
     function name() public pure override(ERC721Upgradeable, IKintoAppRegistry) returns (string memory) {
         return "Kinto APP";
     }
 
     /**
-     * @dev Gets the token symbol.
-     * @return string representing the token symbol
+     * @notice Gets the token symbol
+     * @return The symbol of the token
      */
     function symbol() public pure override(ERC721Upgradeable, IKintoAppRegistry) returns (string memory) {
         return "KINTOAPP";
     }
 
     /**
-     * @dev Returns the base token URI. ID is appended
-     * @return token URI.
+     * @notice Returns the base token URI
+     * @return The base token URI
      */
     function _baseURI() internal pure override returns (string memory) {
         return "https://kinto.xyz/metadata/kintoapp/";
@@ -122,15 +156,15 @@ contract KintoAppRegistry is
     /* ============ App Registration ============ */
 
     /**
-     * @dev Register a new app and mints the NFT to the creator
-     * @param _name The name of the app
+     * @notice Registers a new app and mints the NFT to the creator
+     * @param name The name of the app
      * @param parentContract The address of the parent contract
      * @param appContracts The addresses of the child contracts
      * @param appLimits The limits of the app
      * @param devEOAs The addresses of the developers EOAs to be whitelisted
      */
     function registerApp(
-        string calldata _name,
+        string calldata name,
         address parentContract,
         address[] calldata appContracts,
         uint256[4] calldata appLimits,
@@ -142,22 +176,22 @@ contract KintoAppRegistry is
         if (walletFactory.walletTs(parentContract) != 0) revert CannotRegisterWallet();
 
         appCount++;
-        _updateMetadata(appCount, _name, parentContract, appContracts, appLimits, devEOAs);
+        _updateMetadata(appCount, name, parentContract, appContracts, appLimits, devEOAs);
         _safeMint(msg.sender, appCount);
 
         emit AppRegistered(parentContract, msg.sender, block.timestamp);
     }
 
     /**
-     * @dev Allows the developer to update the metadata of the app
-     * @param _name The name of the app
+     * @notice Allows the developer to update the metadata of the app
+     * @param name The name of the app
      * @param parentContract The address of the parent contract
      * @param appContracts The addresses of the child contracts
      * @param appLimits The limits of the app
      * @param devEOAs The addresses of the developers EOAs to be whitelisted
      */
     function updateMetadata(
-        string calldata _name,
+        string calldata name,
         address parentContract,
         address[] calldata appContracts,
         uint256[4] calldata appLimits,
@@ -165,13 +199,13 @@ contract KintoAppRegistry is
     ) external override {
         uint256 tokenId = _appMetadata[parentContract].tokenId;
         if (msg.sender != ownerOf(tokenId)) revert OnlyAppDeveloper();
-        _updateMetadata(tokenId, _name, parentContract, appContracts, appLimits, devEOAs);
+        _updateMetadata(tokenId, name, parentContract, appContracts, appLimits, devEOAs);
 
         emit AppUpdated(parentContract, msg.sender, block.timestamp);
     }
 
     /**
-     * @dev Allows the developer to set sponsored contracts
+     * @notice Allows the developer to set sponsored contracts
      * @param _app The address of the app
      * @param _contracts The addresses of the contracts
      * @param _flags The flags of the contracts
@@ -193,7 +227,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Allows the app to request PII data
+     * @notice Allows the app to request PII data
      * @param app The name of the app
      */
     function enableDSA(address app) external override onlyOwner {
@@ -203,7 +237,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Allows the owner to override the parent contract of a child contract
+     * @notice Allows the owner to override the parent contract of a child contract
      * @param child The address of the child contract
      * @param parent The address of the parent contract
      */
@@ -212,7 +246,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Updates the system contracts array
+     * @notice Updates the system contracts array
      * @param newSystemContracts The new array of system contracts
      */
     function updateSystemContracts(address[] calldata newSystemContracts) external onlyOwner {
@@ -226,19 +260,22 @@ contract KintoAppRegistry is
         systemContracts = newSystemContracts;
     }
 
+    /**
+     * @notice Sets the deployer EOA for a wallet
+     * @param deployer The address of the deployer EOA
+     */
     function setDeployerEOA(address deployer) external {
         address wallet = msg.sender;
-        if(walletFactory.walletTs(wallet) == 0) revert InvalidWallet(msg.sender);
+        if (walletFactory.walletTs(wallet) == 0) revert InvalidWallet(msg.sender);
 
         emit DeployerSet(deployer);
         deployerToWallet[deployer] = wallet;
-
     }
 
     /* ============ Getters ============ */
 
     /**
-     * @dev Returns whether the contract implements the interface defined by the id
+     * @notice Returns whether the contract implements the interface defined by the id
      * @param interfaceId id of the interface to be checked.
      * @return true if the contract implements the interface defined by the id.
      */
@@ -252,7 +289,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Returns the metadata of the app
+     * @notice Returns the metadata of the app
      * @param _contract The address of the app
      * @return The metadata of the app
      */
@@ -262,7 +299,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Returns the limits of the app
+     * @notice Returns the limits of the app
      * @param _contract The address of the app
      * @return The limits of the app
      */
@@ -278,7 +315,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Returns whether a contract is sponsored by an app
+     * @notice Returns whether a contract is sponsored by an app
      * @param _app The address of the app
      * @param _contract The address of the contract
      * @return bool true or false
@@ -288,7 +325,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Returns the sponsoring contract for a given contract (aka parent contract)
+     * @notice Returns the sponsoring contract for a given contract (aka parent contract)
      * @param _contract The address of the contract
      * @return The address of the contract that sponsors the contract
      */
@@ -319,7 +356,7 @@ contract KintoAppRegistry is
         if (to == address(0) || to == CREATE2) {
             address wallet = deployerToWallet[from];
             // Only dev wallets can deploy
-            if(wallet == address(0)) return false;
+            if (wallet == address(0)) return false;
             // Deny if wallet has no KYC
             if (!kintoID.isKYC(IKintoWallet(wallet).owners(0))) return false;
             // Permit if EOA have a wallet, dev mode and KYC
@@ -341,9 +378,18 @@ contract KintoAppRegistry is
 
     /* =========== Internal methods =========== */
 
+    /**
+     * @notice Updates the metadata of an app
+     * @param tokenId The token ID of the app
+     * @param name The name of the app
+     * @param parentContract The address of the parent contract
+     * @param appContracts The addresses of the child contracts
+     * @param appLimits The limits of the app
+     * @param devEOAs The addresses of the developers EOAs to be whitelisted
+     */
     function _updateMetadata(
         uint256 tokenId,
-        string calldata _name,
+        string calldata name,
         address parentContract,
         address[] calldata appContracts,
         uint256[4] calldata appLimits,
@@ -363,7 +409,7 @@ contract KintoAppRegistry is
 
         IKintoAppRegistry.Metadata memory metadata = IKintoAppRegistry.Metadata({
             tokenId: tokenId,
-            name: _name,
+            name: name,
             dsaEnabled: false,
             rateLimitPeriod: appLimits[0],
             rateLimitNumber: appLimits[1],
@@ -389,7 +435,7 @@ contract KintoAppRegistry is
     }
 
     /**
-     * @dev Hook that is called before any token transfer. Allow only mints and burns, no transfers.
+     * @notice Hook that is called before any token transfer. Allow only mints and burns, no transfers
      * @param from source address
      * @param to target address
      * @param batchSize The first id
