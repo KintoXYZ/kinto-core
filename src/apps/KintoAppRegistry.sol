38c9b7743361df2f38adc83e49b3dcec7bf52f61
Igor Yalovoy <to@ylv.io>
2024-07-12 09:08:45 -0600

    Refactor import statement for ERC721EnumerableUpgradeable to improve readability.

--- src/apps/KintoAppRegistry.sol
+++ src/apps/KintoAppRegistry.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import {ERC721EnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import {ERC721EnumerableUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
