e6464787b6947964797f0363659b0b1c83e5d9e1
Igor Yalovoy <to@ylv.io>
2024-07-15 14:54:55 -0600

    Added new migration script for upgrading RewardsDistributor to V4 and updated addresses.json with the new implementation address.

---
+++ script/migrations/95-upgrade_distr.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {IKintoWallet} from "@kinto-core/interfaces/IKintoWallet.sol";
+import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
+import {RewardsDistributor} from "@kinto-core/liquidity-mining/RewardsDistributor.sol";
+
+contract UpgradeDistributorScript is MigrationHelper {
+    function run() public override {
+        super.run();
+
+        uint256 LIQUIDITY_MINING_START_DATE = 1718690400; // June 18th 2024
+
+        bytes memory bytecode = abi.encodePacked(
+            type(RewardsDistributor).creationCode,
+            abi.encode(_getChainDeployment("KINTO"), _getChainDeployment("EngenCredits"), LIQUIDITY_MINING_START_DATE)
+        );
+
+        address impl = _deployImplementationAndUpgrade("RewardsDistributor", "V4", bytecode, keccak256("V4"));
+
+        RewardsDistributor distr = RewardsDistributor(_getChainDeployment("RewardsDistributor"));
+
+        assertEq(address(distr.KINTO()), 0x010700808D59d2bb92257fCafACfe8e5bFF7aB87);
+        assertEq(address(distr.ENGEN()), 0xD1295F0d8789c3E0931A04F91049dB33549E9C8F);
+        assertEq(distr.startTime(), LIQUIDITY_MINING_START_DATE);
+
+        saveContractAddress("RewardsDistributorV4-impl", impl);
+    }
+}
