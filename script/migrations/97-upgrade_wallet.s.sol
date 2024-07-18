28602b27f558968df1bc8f3a14b3ff4df0e19081
Igor Yalovoy <to@ylv.io>
2024-07-17 22:40:48 -0300

    Admin wallet reset signers (#237)
    * Add more bridged tokens
    
    * Add new path mappings for kinto-core script, test, and src directories in KintoWallet.conf
    
    * Remove duplicate entry for account-abstraction contracts in KintoWallet.conf.
    
    * Updated KintoWallet.conf to include additional package paths and reorganize existing ones for better dependency management.
    
    * Added multiple new package paths to Certora configuration files for various KintoID and SponsorPaymaster specs.
    
    * Remove commented-out code and add new migration script for resetting wallet signers in multisig and upgrade wallet scripts.
    
    * Add new JSON files for wallet upgrade, update socket app migration script formatting, and adjust KintoAppRegistry contract logic for child contract registration.

--- script/migrations/97-upgrade_wallet.s.sol
+++ script/migrations/97-upgrade_wallet.s.sol
@@ -6,33 +6,21 @@ import "../../src/wallet/KintoWallet.sol";
 import "../../src/sample/Counter.sol";
 import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
 
-contract UpgradeWalletDeployScript is MigrationHelper {
+contract ResetWalletSignersScript is MigrationHelper {
     function run() public override {
         super.run();
 
-        bytes memory bytecode;
-
-        bytecode = abi.encodePacked(
-            type(KintoWallet).creationCode,
-            abi.encode(
-                _getChainDeployment("EntryPoint"),
-                _getChainDeployment("KintoID"),
-                _getChainDeployment("KintoAppRegistry")
-            )
-        );
-
-        address impl = _deployImplementationAndUpgrade("KintoWallet", "V27", bytecode);
-
-        saveContractAddress("KintoWalletV27-impl", impl);
-
         // Add a new signer
-        address[] memory signers = new address[](3);
+        address[] memory signers = new address[](4);
         signers[0] = IKintoWallet(kintoAdminWallet).owners(0);
         signers[1] = IKintoWallet(kintoAdminWallet).owners(1);
-        signers[2] = 0x08E674c4538caE03B6c05405881dDCd95DcaF5a8;
+        signers[2] = IKintoWallet(kintoAdminWallet).owners(2);
+        signers[3] = 0x94561e98DD5E55271f91A103e4979aa6C493745E;
 
         _handleOps(
-            abi.encodeWithSelector(IKintoWallet.resetSigners.selector, signers, IKintoWallet(impl).TWO_SIGNERS()),
+            abi.encodeWithSelector(
+                IKintoWallet.resetSigners.selector, signers, IKintoWallet(kintoAdminWallet).TWO_SIGNERS()
+            ),
             kintoAdminWallet
         );
 
