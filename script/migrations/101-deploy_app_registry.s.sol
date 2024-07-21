10d55a5a6c3e99ec1f250073f64a9a6e531995dc
Ramon Recuero <rrmoreno1987@gmail.com>
2024-07-21 10:31:48 -0700

    Adds KYCViewer

--- script/migrations/101-deploy_app_registry.s.sol
+++ script/migrations/101-deploy_app_registry.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "../../src/wallet/KintoWallet.sol";
 import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
 import {KintoAppRegistry} from "@kinto-core/apps/KintoAppRegistry.sol";
+import {KYCViewer} from "@kinto-core/viewers/KYCViewer.sol";
 
 contract DeployScript is MigrationHelper {
     function run() public override {
@@ -11,9 +12,21 @@ contract DeployScript is MigrationHelper {
 
         bytes memory bytecode =
             abi.encodePacked(type(KintoAppRegistry).creationCode, abi.encode(_getChainDeployment("KintoWalletFactory")));
-        address impl = _deployImplementationAndUpgrade("KintoAppRegistry", "V15", bytecode);
+        address impl = _deployImplementationAndUpgrade("KintoAppRegistry", "V16", bytecode);
 
-        saveContractAddress("KintoAppRegistryV15-impl", impl);
-        KintoAppRegistry kintoAppRegistry = KintoAppRegistry(payable(_getChainDeployment("KintoAppRegistry")));
+        saveContractAddress("KintoAppRegistryV16-impl", impl);
+
+        // bytecode = abi.encodePacked(
+        //     type(KYCViewer).creationCode,
+        //     abi.encode(
+        //         _getChainDeployment("KintoWalletFactory"),
+        //         _getChainDeployment("Faucet"),
+        //         _getChainDeployment("EngenCredits")
+        //     )
+        // );
+
+        // // upgrade KYCViewer to V10
+        // impl = _deployImplementationAndUpgrade("KYCViewer", "V10", bytecode);
+        // saveContractAddress("KintoAppRegistryV10-impl", impl);
     }
 }
