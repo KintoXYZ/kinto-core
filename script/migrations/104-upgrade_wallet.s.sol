893bef03e600562120e9faa532d7ff83fc82e48d
Igor Yalovoy <to@ylv.io>
2024-07-25 19:53:27 -0600

    Added migration script for KintoWallet V30 implementation and updated addresses.json with new contract address.

---
+++ script/migrations/104-upgrade_wallet.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../../src/wallet/KintoWalletFactory.sol";
+import "../../src/wallet/KintoWallet.sol";
+import "../../src/sample/Counter.sol";
+import {MigrationHelper} from "@kinto-core-script/utils/MigrationHelper.sol";
+
+contract UpgradeWalletDeployScript is MigrationHelper {
+    function run() public override {
+        super.run();
+
+        bytes memory bytecode = abi.encodePacked(
+            type(KintoWallet).creationCode,
+            abi.encode(
+                _getChainDeployment("EntryPoint"),
+                _getChainDeployment("KintoID"),
+                _getChainDeployment("KintoAppRegistry"),
+                _getChainDeployment("KintoWalletFactory")
+            )
+        );
+
+        address impl = _deployImplementationAndUpgrade("KintoWallet", "V30", bytecode);
+
+        saveContractAddress("KintoWalletV30-impl", impl);
+    }
+}
