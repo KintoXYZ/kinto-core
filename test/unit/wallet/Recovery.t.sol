9050df06b29656140cd008eef392fd6d64dd160a
Igor Yalovoy <to@ylv.io>
2024-08-26 12:13:38 -0600

    Refactor imports in Recovery.t.sol to use named imports and add IERC20Errors, IKintoWallet, and IKintoID interfaces; update expectRevert with specific error selector.

--- test/unit/wallet/Recovery.t.sol
+++ test/unit/wallet/Recovery.t.sol
@@ -2,10 +2,13 @@
 
 pragma solidity ^0.8.18;
 
-import "@kinto-core-test/SharedSetup.t.sol";
+import {SharedSetup} from "@kinto-core-test/SharedSetup.t.sol";
 import {BridgedKinto} from "@kinto-core/tokens/bridged/BridgedKinto.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20Errors} from "@openzeppelin-5.0.1/contracts/interfaces/draft-IERC6093.sol";
+import {IKintoWallet} from "@kinto-core/interfaces/IKintoWallet.sol";
+import {IKintoID} from "@kinto-core/interfaces/IKintoID.sol";
 
 contract RecoveryTest is SharedSetup {
     using SafeERC20 for IERC20;
@@ -35,7 +38,9 @@ contract RecoveryTest is SharedSetup {
         _kintoWallet.cancelRecovery();
 
         vm.prank(address(_walletFactory));
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodeWithSelector(IERC20Errors.ERC20InsufficientBalance.selector, address(_kintoWallet), 0, 5e18)
+        );
         _kintoWallet.startRecovery();
     }
 
