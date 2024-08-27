83fa561811d340d3d97e62f529116736435b2730
Igor Yalovoy <to@ylv.io>
2024-08-27 12:19:09 -0300

    System apps (#256)
    * Added system app management functionality and refactored sponsor terminology to app in KintoAppRegistry and related contracts.
    
    * Refactor error handling in KintoWallet and update tests to include new AppNotSponsored error.
    
    * Refactored KintoWallet execute functions, updated unit tests for execute and executeBatch, and added console2 import for debugging.
    
    * Refactored KintoWallet by removing deprecated socket handling and updated Execute tests to include system app setup and validation.
    
    * Remove privileged app signature validation tests and SOCKET constant from ValidateSignature.t.sol.
    
    * Refactored IKintoWallet and KintoWallet contracts to enhance documentation, add detailed comments, and improve code readability and maintainability.
    
    * Add tests for updateSystemApps function in KintoAppRegistry contract to verify correct behavior and edge cases.
    
    * Removed unused constant addresses: SOCKET, ADMIN_WALLET, and REWARDS_DISTRIBUTOR from KintoWallet.sol.
    
    * Update slither.config.json to exclude node_modules directory from analysis
    
    * Refactor appRegistry method calls from getSponsor to getApp in KintoWallet.spec and setup.spec for consistency.
    
    * Add tests for Paymaster and executeBatch functionalities, including scenarios for non-whitelisted and non-sponsored apps.

--- test/unit/wallet/ExecuteBatch.t.sol
+++ test/unit/wallet/ExecuteBatch.t.sol
@@ -5,29 +5,38 @@ pragma solidity ^0.8.18;
 import "@aa/interfaces/IEntryPoint.sol";
 
 import "@kinto-core-test/SharedSetup.t.sol";
+import {IKintoAppRegistry} from "@kinto-core/interfaces/IKintoAppRegistry.sol";
 
 contract ExecuteBatchTest is SharedSetup {
+    /* ============ Paymaster ============ */
+
     function testExecuteBatch_WhenPaymaster() public {
-        // prep batch
-        address[] memory targets = new address[](1);
-        targets[0] = address(counter);
+        Counter counter2 = new Counter();
 
-        uint256[] memory values = new uint256[](1);
+        address[] memory targets = new address[](3);
+        targets[0] = address(_kintoWallet);
+        targets[1] = address(counter2);
+        targets[2] = address(counter2);
+
+        uint256[] memory values = new uint256[](3);
         values[0] = 0;
+        values[1] = 0;
+        values[2] = 0;
 
-        // we want to do 3 calls: whitelistApp, increment counter and increment counter2
-        bytes[] memory calls = new bytes[](1);
-        calls[0] = abi.encodeWithSignature("increment()");
+        address[] memory whitelistTargets = new address[](1);
+        whitelistTargets[0] = address(counter2);
+        bool[] memory flags = new bool[](1);
+        flags[0] = true;
 
-        OperationParamsBatch memory opParams = OperationParamsBatch({targets: targets, values: values, bytesOps: calls});
-        UserOperation memory userOp = _createUserOperation(
-            address(_kintoWallet), _kintoWallet.getNonce(), privateKeys, opParams, address(_paymaster)
-        );
-        UserOperation[] memory userOps = new UserOperation[](1);
-        userOps[0] = userOp;
+        bytes[] memory calls = new bytes[](3);
+        calls[0] = abi.encodeWithSelector(IKintoWallet.whitelistApp.selector, whitelistTargets, flags);
+        calls[1] = abi.encodeWithSignature("increment()");
+        calls[2] = abi.encodeWithSignature("increment()");
 
-        _entryPoint.handleOps(userOps, payable(_owner));
-        assertEq(counter.count(), 1);
+        vm.prank(address(_entryPoint));
+        _kintoWallet.executeBatch(targets, values, calls);
+
+        assertEq(counter2.count(), 2);
     }
 
     function testExecuteBatch_RevertWhen_NoPaymasterNorPrefund() public {
@@ -75,6 +84,7 @@ contract ExecuteBatchTest is SharedSetup {
             _createUserOperation(address(_kintoWallet), _kintoWallet.getNonce(), privateKeys, opParams, address(0));
 
         _entryPoint.handleOps(userOps, payable(_owner));
+
         assertEq(counter.count(), 1);
     }
 
@@ -102,6 +112,8 @@ contract ExecuteBatchTest is SharedSetup {
         assertEq(counter.count(), 2);
     }
 
+    /* ============ executeBatch ============ */
+
     function testExecuteBatch_RevertWhen_AppIsNotWhitelisted() public {
         // remove app from whitelist
         whitelistApp(address(counter), false);
@@ -119,19 +131,11 @@ contract ExecuteBatchTest is SharedSetup {
         calls[0] = abi.encodeWithSignature("recoverer()");
         calls[1] = abi.encodeWithSignature("increment()");
 
-        OperationParamsBatch memory opParams = OperationParamsBatch({targets: targets, values: values, bytesOps: calls});
-        UserOperation[] memory userOps = new UserOperation[](1);
-        userOps[0] = _createUserOperation(
-            address(_kintoWallet), _kintoWallet.getNonce(), privateKeys, opParams, address(_paymaster)
+        vm.prank(address(_entryPoint));
+        vm.expectRevert(
+            abi.encodeWithSelector(IKintoWallet.AppNotWhitelisted.selector, address(counter), address(counter))
         );
-
-        vm.expectEmit(true, true, true, false);
-        emit UserOperationRevertReason(
-            _entryPoint.getUserOpHash(userOps[0]), userOps[0].sender, userOps[0].nonce, bytes("")
-        );
-        vm.recordLogs();
-        _entryPoint.handleOps(userOps, payable(_owner));
-        assertRevertReasonEq(IKintoWallet.AppNotWhitelisted.selector);
+        _kintoWallet.executeBatch(targets, values, calls);
     }
 
     function testExecuteBatch_RevertWhen_LengthMismatch() public {
@@ -194,5 +198,33 @@ contract ExecuteBatchTest is SharedSetup {
         vm.recordLogs();
         _entryPoint.handleOps(userOps, payable(_owner));
         assertRevertReasonEq(IKintoWallet.LengthMismatch.selector);
+    }
+
+    function testExecuteBatch_RevertWhen_AppNotSponsored() public {
+        address notSponsored = address(new Counter());
+
+        // Prepare batch execution data
+        address[] memory targets = new address[](2);
+        targets[0] = notSponsored; // This is sponsored
+        targets[1] = address(counter); // This is the app, but not sponsored
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 0;
+        values[1] = 0;
+
+        bytes[] memory callData = new bytes[](2);
+        callData[0] = abi.encodeWithSelector(Counter.increment.selector);
+        callData[1] = abi.encodeWithSelector(Counter.increment.selector);
+
+        // Attempt to execute the batch
+        vm.prank(address(_entryPoint));
+        vm.expectRevert(
+            abi.encodeWithSelector(IKintoWallet.AppNotSponsored.selector, address(counter), address(notSponsored))
+        );
+        _kintoWallet.executeBatch(targets, values, callData);
+
+        // Verify that no counters were incremented
+        assertEq(Counter(notSponsored).count(), 0);
+        assertEq(counter.count(), 0);
     }
 }
--- test/unit/wallet/Whitelist.t.sol
+++ test/unit/wallet/Whitelist.t.sol
@@ -6,44 +6,24 @@ import "@kinto-core-test/SharedSetup.t.sol";
 
 contract WhitelistTest is SharedSetup {
     function testWhitelistAppAndSetKey() public {
-        // deploy an app
         Counter counter = new Counter();
-        assertEq(counter.count(), 0);
 
-        UserOperation[] memory userOps = new UserOperation[](1);
-        userOps[0] = _createUserOperation(
-            address(_kintoWallet),
-            address(_kintoWallet), // target is the wallet itself
-            _kintoWallet.getNonce(),
-            privateKeys,
-            abi.encodeWithSignature("whitelistAppAndSetKey(address,address)", address(counter), _user),
-            address(_paymaster)
-        );
-
-        _entryPoint.handleOps(userOps, payable(_owner));
+        vm.prank(address(_kintoWallet));
+        _kintoWallet.whitelistAppAndSetKey(address(counter), _user);
 
         assertTrue(_kintoWallet.appWhitelist(address(counter)));
         assertEq(_kintoWallet.appSigner(address(counter)), _user);
     }
 
     function testWhitelistApp() public {
-        // deploy an app
         Counter counter = new Counter();
-        assertEq(counter.count(), 0);
 
-        // create whitelist app user op
         whitelistApp(address(counter), true);
 
         assertTrue(_kintoWallet.appWhitelist(address(counter)));
     }
 
-    function testWhitelistApp_RevertWhen_AlreadyWhitelisted() public {
-        //re-register app
-        whitelistApp(address(counter), true);
-    }
-
     function testWhitelistAppRemovesAppKey() public {
-        // deploy an app
         Counter counter = new Counter();
 
         whitelistApp(address(counter), true);
@@ -51,16 +31,9 @@ contract WhitelistTest is SharedSetup {
         assertTrue(_kintoWallet.appWhitelist(address(counter)));
         assertEq(_kintoWallet.appSigner(address(counter)), address(0), "Signer is not a zero address");
 
-        UserOperation[] memory userOps = new UserOperation[](1);
-        userOps[0] = _createUserOperation(
-            address(_kintoWallet),
-            address(_kintoWallet),
-            _kintoWallet.getNonce(),
-            privateKeys,
-            abi.encodeWithSignature("setAppKey(address,address)", address(counter), _user),
-            address(_paymaster)
-        );
-        _entryPoint.handleOps(userOps, payable(_owner));
+        vm.prank(address(_kintoWallet));
+        _kintoWallet.setAppKey(address(counter), _user);
+
         assertEq(_kintoWallet.appSigner(address(counter)), _user);
 
         whitelistApp(address(counter), false);
@@ -68,28 +41,4 @@ contract WhitelistTest is SharedSetup {
         assertFalse(_kintoWallet.appWhitelist(address(counter)));
         assertEq(_kintoWallet.appSigner(address(counter)), address(0), "Signer is not a zero address");
     }
-
-    // function testWhitelist_RevertWhen_AppIsNotRegistered() public {
-    //     // (1). deploy Counter contract
-    //     Counter counter = new Counter();
-    //     assertEq(counter.count(), 0);
-
-    //     // (2). fund paymaster for Counter contract
-    //     fundSponsorForApp(_owner, address(counter));
-
-    //     // (3). Create whitelist app user op
-    //     UserOperation[] memory userOps = new UserOperation[](1);
-    //     userOps[0] = _whitelistAppOp(
-    //         privateKeys, address(_kintoWallet), _kintoWallet.getNonce(), address(counter), address(_paymaster)
-    //     );
-
-    //     // (4). execute the transaction via the entry point and expect a revert event
-    //     vm.expectEmit(true, true, true, false);
-    //     emit UserOperationRevertReason(
-    //         _entryPoint.getUserOpHash(userOps[0]), userOps[0].sender, userOps[0].nonce, bytes("")
-    //     );
-    //     vm.recordLogs();
-    //     _entryPoint.handleOps(userOps, payable(_owner));
-    //     assertRevertReasonEq("KW-apw: app must be registered");
-    // }
 }
