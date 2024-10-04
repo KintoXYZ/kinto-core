0ada1f20f958cab8dfb00d252ee21dc375247bc0
Igor Yalovoy <to@ylv.io>
2024-10-04 16:55:30 -0600

    EIGEN and ETHFI drop

--- utils/eigen-distr.js
+++ utils/eigen-distr.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parse/sync');
 
 // Read the CSV file
-const input = fs.readFileSync('./script/data/ETHFI_finalv2_distribution.csv', 'utf8');
+const input = fs.readFileSync('./script/data/EIGEN_finalv2_distribution.csv', 'utf8');
 
 // Parse the CSV data
 const records = csv.parse(input, {
@@ -18,13 +18,13 @@ records.forEach(record => {
   console.log('record:', record)
   const wallet = record['wallet'];
   console.log('wallet:', wallet)
-  const amountStr = record['ETHFI'];
+  const amountStr = record['EIGEN'];
   // Remove the comma and convert to cents (multiply by 100)
-  // 5,294.84289046560000000
-  const valueInCents = BigInt(Math.round(parseFloat(amountStr.replace(",", "")) * 1e17));
+  // 4,204.373759065820000
+  const valueInCents = BigInt(Math.round(parseFloat(amountStr.replace(",", "")) * 1e15));
 
-  // Multiply by 10 to get to 1e18 (since we're already at 1e17 cents)
-  const amount = valueInCents * BigInt(10);
+  // Multiply by 10e3 to get to 1e18 (since we're already at 1e15)
+  const amount = valueInCents * BigInt(10 ** 3);
   console.log('amount:', amount)
 
   totalTokens += amount;
@@ -38,6 +38,6 @@ records.forEach(record => {
 console.log('totalTokens:', totalTokens)
 
 // Write the output to a JSON file
-fs.writeFileSync('./script/data/ETHFI_finalv2_distribution.json', JSON.stringify(output, null, 2));
+fs.writeFileSync('./script/data/EIGEN_finalv2_distribution.json', JSON.stringify(output, null, 2));
 
 console.log('Conversion complete. Check output.json for the result.');
