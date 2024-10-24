b7299804db30a5dd4f60bbf196f4877aede8b81c
Igor Yalovoy <to@ylv.io>
2024-10-24 15:49:45 -0600

    Add migration script for transferring treasury ownership and update constants with NIO roles and execution delay.

--- script/migrations/const.sol
+++ script/migrations/const.sol
@@ -5,4 +5,6 @@ contract Constants {
     uint256 internal constant LEDGER = 0;
     uint256 internal constant TREZOR = 1;
     address WUSDM = 0x57F5E098CaD7A3D1Eed53991D4d66C45C9AF7812; // wUSDM
+    uint64 internal constant NIO_GOVERNOR_ROLE = uint64(uint256(keccak256("NIO_GOVERNOR_ROLE")));
+    uint256 internal constant NIO_EXECUTION_DELAY = 3 days;
 }
