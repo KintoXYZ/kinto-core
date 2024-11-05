a18e9e30286a53ed45c796cdba589d8507825bc1
Igor Yalovoy <to@ylv.io>
2024-11-05 09:01:19 -0600

    Refactor token deployment to support Ethereum, Arbitrum, and Base networks for Aave tokens.

--- script/migrations/141-aave-lend-tokens.s.sol
+++ script/migrations/141-aave-lend-tokens.s.sol
@@ -21,32 +21,60 @@ contract Script is MigrationHelper {
         super.run();
 
         // ETH
-        deployBridgedToken("aETH", "Aave WETH", 18, "AE7000");
+        deployBridgedToken("aEthWETH", "Aave Ethereum WETH", 18, "AE7000");
+        deployBridgedToken("aArbWETH", "Aave Arbitrum WETH", 18, "AE7000");
+        deployBridgedToken("aBasWETH", "Aave Base WETH", 18, "AE7000");
         // WBTC
-        deployBridgedToken("aWBTC", "Aave WBTC ", 8, "A0B7C0");
+        deployBridgedToken("aEthWBTC", "Aave Ethereum WBTC ", 8, "A0B7C0");
+        deployBridgedToken("aArbWBTC", "Aave Arbitrum WBTC ", 8, "A0B7C0");
+        deployBridgedToken("aBasWBTC", "Aave Base WBTC ", 8, "A0B7C0");
         // weETH
-        deployBridgedToken("aweETH", "Aave weETH", 18, "A0EE70");
+        deployBridgedToken("aweEthETH", "Aave Ethereum weETH", 18, "A0EE70");
+        deployBridgedToken("aweArbETH", "Aave Arbitrum weETH", 18, "A0EE70");
+        deployBridgedToken("aweBasETH", "Aave Base weETH", 18, "A0EE70");
         // USDC
-        deployBridgedToken("aUSDC", "Aave weETH", 6, "A06DC0");
+        deployBridgedToken("aEthUSDC", "Aave Ethereum weETH", 6, "A06DC0");
+        deployBridgedToken("aArbUSDC", "Aave Arbitrum weETH", 6, "A06DC0");
+        deployBridgedToken("aBasUSDC", "Aave Base weETH", 6, "A06DC0");
         // USDT
-        deployBridgedToken("aUSDT", "Aave USDT", 6, "A06D70");
+        deployBridgedToken("aEthUSDT", "Aave Ethereum USDT", 6, "A06D70");
+        deployBridgedToken("aArbUSDT", "Aave Arbitrum USDT", 6, "A06D70");
+        deployBridgedToken("aBasUSDT", "Aave Base USDT", 6, "A06D70");
         // ARB
-        deployBridgedToken("aARB", "Aave ARB", 18, "AA9B00");
+        deployBridgedToken("aEthARB", "Aave Ethereum ARB", 18, "AA9B00");
+        deployBridgedToken("aArbARB", "Aave Arbitrum ARB", 18, "AA9B00");
+        deployBridgedToken("aBasARB", "Aave Base ARB", 18, "AA9B00");
         // DAI
-        deployBridgedToken("aDAI", "Aave DAI", 18, "ADA100");
+        deployBridgedToken("aEthDAI", "Aave Ethereum DAI", 18, "ADA100");
+        deployBridgedToken("aArbDAI", "Aave Arbitrum DAI", 18, "ADA100");
+        deployBridgedToken("aBasDAI", "Aave Base DAI", 18, "ADA100");
         // LINK
-        deployBridgedToken("aLINK", "Aave LINK", 18, "A11000");
+        deployBridgedToken("aEthLINK", "Aave Ethereum LINK", 18, "A11000");
+        deployBridgedToken("aArbLINK", "Aave Arbitrum LINK", 18, "A11000");
+        deployBridgedToken("aBasLINK", "Aave Base LINK", 18, "A11000");
         // GHO
-        deployBridgedToken("aGHO", "Aave Gho", 18, "A60000");
+        deployBridgedToken("aEthGHO", "Aave Ethereum Gho", 18, "A60000");
+        deployBridgedToken("aArbGHO", "Aave Arbitrum Gho", 18, "A60000");
+        deployBridgedToken("aBasGHO", "Aave Base Gho", 18, "A60000");
         // rETH
-        deployBridgedToken("arETH", "Aave rETH", 18, "A9E700");
+        deployBridgedToken("arEthETH", "Aave Ethereum rETH", 18, "A9E700");
+        deployBridgedToken("arArbETH", "Aave Arbitrum rETH", 18, "A9E700");
+        deployBridgedToken("arBasETH", "Aave Base rETH", 18, "A9E700");
         // cbETH
-        deployBridgedToken("acbETH", "Aave cbETH", 18, "ACBE70");
+        deployBridgedToken("acbEthETH", "Aave Ethereum cbETH", 18, "ACBE70");
+        deployBridgedToken("acbArbETH", "Aave Arbitrum cbETH", 18, "ACBE70");
+        deployBridgedToken("acbBasETH", "Aave Base cbETH", 18, "ACBE70");
         // cbBTC
-        deployBridgedToken("acbBTC", "Aave cbBTC", 8, "ACBB7C");
+        deployBridgedToken("acbEthBTC", "Aave Ethereum cbBTC", 8, "ACBB7C");
+        deployBridgedToken("acbArbBTC", "Aave Arbitrum cbBTC", 8, "ACBB7C");
+        deployBridgedToken("acbBasBTC", "Aave Base cbBTC", 8, "ACBB7C");
         // Aave
-        deployBridgedToken("aAAVE", "Aave AAVE", 18, "AAA0E0");
+        deployBridgedToken("aEthAAVE", "Aave Ethereum AAVE", 18, "AAA0E0");
+        deployBridgedToken("aArbAAVE", "Aave Arbitrum AAVE", 18, "AAA0E0");
+        deployBridgedToken("aBasAAVE", "Aave Base AAVE", 18, "AAA0E0");
         // wstETH
-        deployBridgedToken("awstETH", "Aave wstETH", 18, "A067E7");
+        deployBridgedToken("awstEthETH", "Aave Ethereum wstETH", 18, "A067E7");
+        deployBridgedToken("awstArbETH", "Aave Arbitrum wstETH", 18, "A067E7");
+        deployBridgedToken("awstBasETH", "Aave Base wstETH", 18, "A067E7");
     }
 }
