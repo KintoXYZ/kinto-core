9ea584b43ca0b74b93646f07d4fecc293b366f02
Igor Yalovoy <to@ylv.io>
2024-11-18 12:53:05 -0600

    Upgrade access protocol: replace AaveLendWorkflow and AaveWithdrawWorkflow with AaveBorrowWorkflow and AaveRepayWorkflow, update contract addresses and transaction details across multiple chains, and remove redundant workflow checks in migration script.

--- script/migrations/145-upgrade_access_protocol.s.sol
+++ script/migrations/145-upgrade_access_protocol.s.sol
@@ -35,27 +35,11 @@ contract DeployScript is Script, MigrationHelper {
             return;
         }
 
-        vm.broadcast(deployerPrivateKey);
-        AaveLendWorkflow aaveLendWorkflow = new AaveLendWorkflow(getAavePoolProvider());
-        saveContractAddress("AaveLendWorkflow", address(aaveLendWorkflow));
-
-        vm.broadcast(deployerPrivateKey);
-        registry.allowWorkflow(address(aaveLendWorkflow));
-
-        vm.broadcast(deployerPrivateKey);
         AaveRepayWorkflow aaveRepayWorkflow = new AaveRepayWorkflow(getAavePoolProvider());
         saveContractAddress("AaveRepayWorkflow", address(aaveRepayWorkflow));
 
         vm.broadcast(deployerPrivateKey);
         registry.allowWorkflow(address(aaveRepayWorkflow));
-
-        vm.broadcast(deployerPrivateKey);
-        AaveWithdrawWorkflow aaveWithdrawWorkflow =
-            new AaveWithdrawWorkflow(getAavePoolProvider(), _getChainDeployment("Bridger"));
-        saveContractAddress("AaveWithdrawWorkflow", address(aaveWithdrawWorkflow));
-
-        vm.broadcast(deployerPrivateKey);
-        registry.allowWorkflow(address(aaveWithdrawWorkflow));
 
         vm.broadcast(deployerPrivateKey);
         AaveBorrowWorkflow aaveBorrowWorkflow =
@@ -65,9 +49,7 @@ contract DeployScript is Script, MigrationHelper {
         vm.broadcast(deployerPrivateKey);
         registry.allowWorkflow(address(aaveBorrowWorkflow));
 
-        require(registry.isWorkflowAllowed(address(aaveLendWorkflow)), "Workflow is not set properly");
         require(registry.isWorkflowAllowed(address(aaveRepayWorkflow)), "Workflow is not set properly");
-        require(registry.isWorkflowAllowed(address(aaveWithdrawWorkflow)), "Workflow is not set properly");
         require(registry.isWorkflowAllowed(address(aaveBorrowWorkflow)), "Workflow is not set properly");
     }
 }
