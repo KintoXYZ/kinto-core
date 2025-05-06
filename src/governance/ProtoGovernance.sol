bd635c8c81b7907096e355748bbd2e8243663900
Igor Yalovoy <to@ylv.io>
2025-05-06 13:37:56 -0300

    Add ProtoGovernance contract deployment scripts, update foundry.toml with optimizer settings, and include new contract address in test artifacts. (#367)

---
+++ src/governance/ProtoGovernance.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
+
+/**
+ * @title ProtoGovernance
+ */
+contract ProtoGovernance is Governor, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token) Governor("ProtoGovernance") GovernorVotes(_token) GovernorVotesQuorumFraction(15) {}
+
+    /**
+     * @dev Returns the delay period for voting.
+     */
+    function votingDelay() public pure override returns (uint256) {
+        return 1 days;
+    }
+
+    /**
+     * @dev Returns the voting period.
+     */
+    function votingPeriod() public pure override returns (uint256) {
+        return 3 weeks;
+    }
+
+    /**
+     * @dev Returns the threshold needed to create a proposal
+     */
+    function proposalThreshold() public pure override returns (uint256) {
+        return 5e18;
+    }
+
+    // The following functions are overrides required by Solidity.
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+}
