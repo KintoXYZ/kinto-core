844f863a56c489af9b16d140884f1c7af9c62722
Ramon Recuero <rrmoreno1987@gmail.com>
2025-05-10 08:47:24 -0700

    Fixes script for now

--- utils/export.js
+++ utils/export.js
@@ -61,7 +61,7 @@ function processFiles() {
     const filePath = path.join(dirPath, file);
     const fileExt = path.extname(filePath);
     console.log('Processing file:', filePath);
-    if (fileExt === '' && !filePath.includes('interfaces') && !filePath.includes('libraries')) {
+    if (fileExt === '' && !filePath.includes('interfaces') && !filePath.includes('libraries') && !filePath.includes('socket')) {
       processDirectory(filePath);
     } else if (fileExt === '.sol') {
       const contractName = path.basename(filePath, '.sol');
